3614f396a4df6012302ae6c376a98ffbd40bd5bc
Shane Vitarana <shanev@gmail.com>
2021-11-17 16:54:06 +0000

    Add query msg and response

--- contracts/minter/src/msg.rs
+++ contracts/minter/src/msg.rs
@@ -1,3 +1,4 @@
+use cosmwasm_std::Addr;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
@@ -11,7 +12,6 @@ pub struct InstantiateMsg {
 pub enum ExecuteMsg {
     Increment {},
     Reset { count: i32 },
-    // Instantiate a new NFT contract given a code id
     InitNFTCollection { code_id: u64 },
 }
 
@@ -20,10 +20,16 @@ pub enum ExecuteMsg {
 pub enum QueryMsg {
     // GetCount returns the current count as a json-encoded number
     GetCount {},
+    CollectionsForCreator { creator: Addr },
 }
 
 // We define a custom struct for each query response
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct CountResponse {
     pub count: i32,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct CollectionsForCreatorResponse {
+    pub collections: Vec<Addr>,
 }
