a91a160f4c2a001345dab5f9affba01447c7fa90
Shane Vitarana <shanev@gmail.com>
2021-11-22 21:38:10 -0500

    Update contract logic

--- contracts/sg721/src/msg.rs
+++ contracts/sg721/src/msg.rs
@@ -1,9 +1,8 @@
-use crate::state::Creator;
+use crate::state::Extension;
 
 pub type InstantiateMsg = cw721_base::InstantiateMsg;
 
-// specialize ExecuteMsg with the Creator extention
-pub type Extension = Creator;
+// specialize ExecuteMsg with the CreatorInfo extention
 pub type ExecuteMsg = cw721_base::ExecuteMsg<Extension>;
 
 pub type QueryMsg = cw721_base::QueryMsg;
--- contracts/sg721/src/state.rs
+++ contracts/sg721/src/state.rs
@@ -1,25 +1,17 @@
-use cosmwasm_std::{Addr, Empty};
-use cw_storage_plus::{Item, Map};
+use cosmwasm_std::Addr;
+use cw_storage_plus::Map;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub count: i32,
-    pub owner: Addr,
+pub struct CreatorInfo {
+    // address for the individual or cw4-group
+    pub creator: Addr,
+    pub creator_share: u64,
 }
-
-pub const STATE: Item<State> = Item::new("state");
 
 // sg721 <> creator mapping
-pub const CREATORS: Map<&str, Creator> = Map::new("creators");
+pub const CREATORS: Map<&str, CreatorInfo> = Map::new("creators");
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct Creator {
-    // address for the individual or..
-    // cw4-group that represents the creators and their ownership weights
-    pub creator: Addr,
-    // share of each sale for the total group
-    // individual shares are handled by weights in the cw4-group
-    pub share: u64,
-}
+// extend contract storage with creator info
+pub type Extension = CreatorInfo;
