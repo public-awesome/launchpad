5ed1e704414ba812157d8cbb2686b4143f772996
Shane Vitarana <shanev@gmail.com>
2021-12-02 15:57:47 -0500

    Added payment addresses to royalty info

--- contracts/sg721/src/state.rs
+++ contracts/sg721/src/state.rs
@@ -12,6 +12,10 @@ pub struct Extension {
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct RoyaltyInfo {
+    /// fallback to creator address if doesn't exist
+    pub creator_payment_address: Option<Addr>,
+    /// fallback to owner address if doesn't exist
+    pub owner_payment_address: Option<Addr>,
     pub creator_share: Decimal,
     pub owner_share: Decimal,
 }
