d753902bfc0a28c9cfb322df981c8abf8b74b25a
Jake Hartnell <jake.hartnell@gmail.com>
2021-12-13 12:03:01 -0800

    Add integration test for alternative cw721 contracts
    To make sure the marketplace supports both sg721 and cw721.

--- contracts/marketplace/src/integration_tests.rs
+++ contracts/marketplace/src/integration_tests.rs
@@ -4,11 +4,8 @@ use crate::msg::{ExecuteMsg, QueryMsg};
 use crate::state::{Ask, Bid};
 use cosmwasm_std::{Addr, Empty};
 use cw721::{Cw721QueryMsg, OwnerOfResponse};
-use cw721_base::msg::{
-    ExecuteMsg as Cw721ExecuteMsg, InstantiateMsg as Cw721InstantiateMsg, MintMsg,
-};
+use cw721_base::msg::{ExecuteMsg as Cw721ExecuteMsg, MintMsg};
 use cw_multi_test::{App, BankSudo, Contract, ContractWrapper, Executor, SudoMsg};
-use sg721;
 
 fn mock_app() -> App {
     App::default()
@@ -28,6 +25,15 @@ pub fn contract_sg721() -> Box<dyn Contract<Empty>> {
         sg721::contract::execute,
         sg721::contract::instantiate,
         sg721::contract::query,
+    );
+    Box::new(contract)
+}
+
+pub fn contract_cw721() -> Box<dyn Contract<Empty>> {
+    let contract = ContractWrapper::new(
+        cw721_metadata_onchain::entry::execute,
+        cw721_metadata_onchain::entry::instantiate,
+        cw721_metadata_onchain::entry::query,
     );
     Box::new(contract)
 }
@@ -166,6 +172,135 @@ mod tests {
 
         // Mint NFT for creator
         mint_nft_for_creator(&mut router, &creator, &nft_contract_addr);
+
+        // Creator Authorizes NFT
+        let approve_msg = Cw721ExecuteMsg::<Empty>::Approve {
+            spender: nft_marketplace_addr.to_string(),
+            token_id: TOKEN_ID.to_string(),
+            expires: None,
+        };
+        let res = router.execute_contract(
+            creator.clone(),
+            nft_contract_addr.clone(),
+            &approve_msg,
+            &[],
+        );
+        assert!(res.is_ok());
+
+        // An asking price is made by the creator
+        let ask = Ask {
+            amount: coin(110, NATIVE_TOKEN_DENOM),
+        };
+        let set_ask = ExecuteMsg::SetAsk {
+            collection: nft_contract_addr.clone(),
+            token_id: TOKEN_ID.to_string(),
+            ask,
+        };
+        let res =
+            router.execute_contract(creator.clone(), nft_marketplace_addr.clone(), &set_ask, &[]);
+        assert!(res.is_ok());
+
+        // Bidder makes bid
+        let bid = Bid {
+            amount: coin(100, NATIVE_TOKEN_DENOM),
+            bidder: bidder.clone(),
+            recipient: creator.clone(),
+        };
+        let set_bid_msg = ExecuteMsg::SetBid {
+            collection: nft_contract_addr.clone(),
+            token_id: TOKEN_ID.to_string(),
+            bid: bid.clone(),
+        };
+        let res = router.execute_contract(
+            bidder.clone(),
+            nft_marketplace_addr.clone(),
+            &set_bid_msg,
+            &coins(100, NATIVE_TOKEN_DENOM),
+        );
+        assert!(res.is_ok());
+
+        // Check contract has been paid
+        let contract_balances = router
+            .wrap()
+            .query_all_balances(nft_marketplace_addr.clone())
+            .unwrap();
+        assert_eq!(contract_balances, coins(100, NATIVE_TOKEN_DENOM));
+
+        // Check creator hasn't been paid yet
+        let creator_native_balances = router.wrap().query_all_balances(creator.clone()).unwrap();
+        assert_eq!(
+            creator_native_balances,
+            coins(INITIAL_BALANCE, NATIVE_TOKEN_DENOM)
+        );
+
+        // Creator accepts bid
+        let accept_bid_msg = ExecuteMsg::AcceptBid {
+            collection: nft_contract_addr.clone(),
+            token_id: TOKEN_ID.to_string(),
+            bid,
+        };
+        let res =
+            router.execute_contract(creator.clone(), nft_marketplace_addr, &accept_bid_msg, &[]);
+        assert!(res.is_ok());
+
+        // Check money is transfered
+        let creator_native_balances = router.wrap().query_all_balances(creator).unwrap();
+        assert_eq!(
+            creator_native_balances,
+            coins(INITIAL_BALANCE + 100, NATIVE_TOKEN_DENOM)
+        );
+        let bidder_native_balances = router.wrap().query_all_balances(bidder.clone()).unwrap();
+        assert_eq!(
+            bidder_native_balances,
+            coins(INITIAL_BALANCE - 100, NATIVE_TOKEN_DENOM)
+        );
+
+        // Check NFT is transferred
+        let query_owner_msg = Cw721QueryMsg::OwnerOf {
+            token_id: TOKEN_ID.to_string(),
+            include_expired: None,
+        };
+        let res: OwnerOfResponse = router
+            .wrap()
+            .query_wasm_smart(nft_contract_addr, &query_owner_msg)
+            .unwrap();
+        assert_eq!(res.owner, bidder.to_string());
+    }
+
+    #[test]
+    fn alternative_cw721_happy_path() {
+        let mut router = mock_app();
+
+        // Setup intial accounts, marketplace contract
+        let (_, bidder, creator) = setup_accounts(&mut router).unwrap();
+        let (nft_marketplace_addr, _) = setup_contracts(&mut router, &creator).unwrap();
+
+        // Upload and instantiate cw721_metadata_onchain contract
+        let cw721_id = router.store_code(contract_cw721());
+        let msg = cw721_metadata_onchain::InstantiateMsg {
+            name: String::from("Test Coin"),
+            symbol: String::from("TEST"),
+            minter: creator.to_string(),
+        };
+        let nft_contract_addr = router
+            .instantiate_contract(cw721_id, creator.clone(), &msg, &[], "NFT", None)
+            .unwrap();
+
+        // Mint NFT
+        let mint_for_creator_msg =
+            cw721_metadata_onchain::ExecuteMsg::Mint(cw721_metadata_onchain::MintMsg {
+                token_id: TOKEN_ID.to_string(),
+                owner: creator.to_string(),
+                token_uri: Some("https://starships.example.com/Starship/Enterprise.json".into()),
+                extension: None,
+            });
+        let res = router.execute_contract(
+            creator.clone(),
+            nft_contract_addr.clone(),
+            &mint_for_creator_msg,
+            &[],
+        );
+        assert!(res.is_ok());
 
         // Creator Authorizes NFT
         let approve_msg = Cw721ExecuteMsg::<Empty>::Approve {
