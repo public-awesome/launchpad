da1c1211be53ae1d4f6d15bb2f3b40423261002f
Shane Vitarana <shanev@gmail.com>
2021-12-28 01:21:36 -0500

    Fix schema

--- contracts/sg721-ics721/examples/schema.rs
+++ contracts/sg721-ics721/examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use cw721_ics721::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use sg721_ics721::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
