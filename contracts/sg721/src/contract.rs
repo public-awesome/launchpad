4a463c5a72304ee78c0e55ab9cebdaa60ebbd78e
Jake Hartnell <jake.hartnell@gmail.com>
2021-12-29 12:20:14 -0800

    Implement Royalties

--- contracts/sg721/src/contract.rs
+++ contracts/sg721/src/contract.rs
@@ -25,7 +25,7 @@ pub fn instantiate(
     _env: Env,
     _info: MessageInfo,
     msg: InstantiateMsg,
-) -> StdResult<Response> {
+) -> Result<Response, ContractError> {
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
 
     let info = ContractInfoResponse {
@@ -39,6 +39,11 @@ pub fn instantiate(
     Sg721Contract::default()
         .minter
         .save(deps.storage, &minter)?;
+
+    // Check royalty info is valid
+    if let Some(ref royalty) = msg.collection_info.royalties {
+        royalty.is_valid()?;
+    }
 
     COLLECTION_INFO.save(deps.storage, &msg.collection_info)?;
 
@@ -83,12 +88,14 @@ fn query_royalties(deps: Deps) -> StdResult<RoyaltyResponse> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
     use crate::state::CollectionInfo;
+    use crate::state::RoyaltyInfo;
     use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-    use cosmwasm_std::{coins, from_binary, Addr};
+    use cosmwasm_std::{coins, from_binary, Addr, Decimal};
 
     #[test]
-    fn proper_initialization() {
+    fn proper_initialization_no_royalties() {
         let mut deps = mock_dependencies();
         let creator = String::from("creator");
         let collection = String::from("collection0");
@@ -124,5 +131,53 @@ mod tests {
         let res = query(deps.as_ref(), mock_env(), QueryMsg::Royalties {}).unwrap();
         let value: RoyaltyResponse = from_binary(&res).unwrap();
         assert_eq!(None, value.royalty);
+    }
+
+    #[test]
+    fn proper_initialization_with_royalties() {
+        let mut deps = mock_dependencies();
+        let creator = String::from("creator");
+        let collection = String::from("collection0");
+
+        let msg = InstantiateMsg {
+            name: collection,
+            symbol: String::from("BOBO"),
+            minter: String::from("minter"),
+            collection_info: CollectionInfo {
+                contract_uri: String::from("https://bafyreibvxty5gjyeedk7or7tahyrzgbrwjkolpairjap3bmegvcjdipt74.ipfs.dweb.link/metadata.json"),
+                creator: Addr::unchecked(creator.clone()),
+                royalties: Some(RoyaltyInfo {
+                    payment_address: Addr::unchecked(creator.clone()),
+                    share: Decimal::percent(10),
+                }),
+            },
+        };
+        let info = mock_info("creator", &coins(1000, "earth"));
+
+        // make sure instantiate doesn't send any messages
+        let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+        assert_eq!(0, res.messages.len());
+
+        // it worked, let's query the contract_uri
+        let res = query(deps.as_ref(), mock_env(), QueryMsg::ContractUri {}).unwrap();
+        let value: ContractUriResponse = from_binary(&res).unwrap();
+        assert_eq!("https://bafyreibvxty5gjyeedk7or7tahyrzgbrwjkolpairjap3bmegvcjdipt74.ipfs.dweb.link/metadata.json", value.contract_uri);
+
+        // it worked, let's query the creator
+        let res = query(deps.as_ref(), mock_env(), QueryMsg::Creator {}).unwrap();
+
+        let value: CreatorResponse = from_binary(&res).unwrap();
+        assert_eq!("creator", value.creator.to_string());
+
+        // let's query the royalties
+        let res = query(deps.as_ref(), mock_env(), QueryMsg::Royalties {}).unwrap();
+        let value: RoyaltyResponse = from_binary(&res).unwrap();
+        assert_eq!(
+            Some(RoyaltyInfo {
+                payment_address: Addr::unchecked(creator),
+                share: Decimal::percent(10),
+            }),
+            value.royalty
+        );
     }
 }
--- contracts/sg721/src/state.rs
+++ contracts/sg721/src/state.rs
@@ -1,3 +1,4 @@
+use crate::ContractError;
 use cosmwasm_std::{Addr, Decimal};
 use cw_storage_plus::Item;
 use schemars::JsonSchema;
@@ -12,12 +13,23 @@ pub struct CollectionInfo {
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct RoyaltyInfo {
-    /// fallback to creator address if doesn't exist
-    pub creator_payment_address: Option<Addr>,
-    /// fallback to owner address if doesn't exist
-    pub owner_payment_address: Option<Addr>,
-    pub creator_share: Decimal,
-    pub owner_share: Decimal,
+    pub payment_address: Addr,
+    pub share: Decimal,
+}
+
+impl RoyaltyInfo {
+    // Checks if RoyaltyInfo is valid
+    pub fn is_valid(&self) -> Result<bool, ContractError> {
+        if self.share > Decimal::one() {
+            return Err(ContractError::InvalidRoyalities {});
+        }
+
+        if self.share < Decimal::zero() {
+            return Err(ContractError::InvalidRoyalities {});
+        }
+
+        Ok(true)
+    }
 }
 
 pub const COLLECTION_INFO: Item<CollectionInfo> = Item::new("collection_info");
