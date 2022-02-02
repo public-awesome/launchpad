525b0f8b9ae88dcd743a93d0bab3e7205f2f8acf
Shane Vitarana <shanev@gmail.com>
2022-02-02 18:57:02 -0500

    Add test

--- contracts/sale/src/bech32.rs
+++ contracts/sale/src/bech32.rs
@@ -1,4 +1,4 @@
-use bech32::{convert_bits, decode, encode, Error, ToBase32, Variant};
+use bech32::{convert_bits, decode, encode, CheckBase32, Error, Variant};
 
 // decodes a bech32 encoded string and converts to base64 encoded bytes
 fn decode_and_convert(bech: &str) -> Result<(String, Vec<u8>), Error> {
@@ -13,15 +13,20 @@ fn decode_and_convert(bech: &str) -> Result<(String, Vec<u8>), Error> {
 // converts from a base64 encoded byte string to base32 encoded byte string and then to bech32
 fn convert_and_encode(hrp: String, data: Vec<u8>) -> Result<String, Error> {
     let converted = convert_bits(&data, 8, 5, true)?;
-    encode(&hrp, converted.to_base32(), Variant::Bech32)
+    encode(&hrp, converted.check_base32()?, Variant::Bech32)
 }
 
-#[test]
-fn to_stars() {
-    let decoded = decode_and_convert("cosmos1p655nr52wepstj39jr3skwv6nkmdrfka99j802").unwrap();
-    let addr = convert_and_encode("stars".to_string(), decoded.1).unwrap();
-    assert_eq!(
-        addr,
-        "stars1p655nr52wepstj39jr3skwv6nkmdrfka3e96ym".to_string()
-    );
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn to_stars() {
+        let decoded = decode_and_convert("cosmos1ey69r37gfxvxg62sh4r0ktpuc46pzjrmz29g45").unwrap();
+        let addr = convert_and_encode("stars".to_string(), decoded.1).unwrap();
+        assert_eq!(
+            addr,
+            "stars1ey69r37gfxvxg62sh4r0ktpuc46pzjrmkkj479".to_string()
+        );
+    }
 }
