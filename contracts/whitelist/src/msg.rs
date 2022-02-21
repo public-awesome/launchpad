d9c523e6125fc817d12179d3fa9ebed67b559453
Shane Vitarana <shanev@gmail.com>
2022-02-21 00:09:49 -0500

    Removed minter

--- contracts/whitelist/src/msg.rs
+++ contracts/whitelist/src/msg.rs
@@ -4,8 +4,6 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
-    // The minter contract is the only one that can update the whitelist
-    pub minter: Option<String>,
     pub members: Vec<String>,
     pub end_time: Expiration,
 }
