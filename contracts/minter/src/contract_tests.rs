7778205a7cb87473fcac87ac666b25a2d9b09103
Shane Vitarana <shanev@gmail.com>
2022-02-26 12:03:13 -0500

    Fix batch mint tests

--- contracts/minter/src/contract_tests.rs
+++ contracts/minter/src/contract_tests.rs
@@ -651,148 +651,129 @@ fn check_per_address_limit() {
     assert!(res.is_err());
 }
 
-// #[test]
-// fn batch_mint_limit_access_max_sold_out() {
-//     let mut router = custom_mock_app();
-//     let (creator, buyer) = setup_accounts(&mut router).unwrap();
-//     let num_tokens = 4;
-//     let (minter_addr, _config) = setup_minter_contract(&mut router, &creator, num_tokens).unwrap();
-//     // set to genesis mint start time
-//     setup_block_time(&mut router, GENESIS_MINT_START_TIME).unwrap();
+#[test]
+fn batch_mint_limit_access_max_sold_out() {
+    let mut router = custom_mock_app();
+    let (creator, buyer) = setup_accounts(&mut router).unwrap();
+    let num_tokens = 4;
+    let (minter_addr, _config) = setup_minter_contract(&mut router, &creator, num_tokens).unwrap();
+    // set to genesis mint start time
+    setup_block_time(&mut router, GENESIS_MINT_START_TIME).unwrap();
 
-//     // batch mint limit set to STARTING_BATCH_MINT_LIMIT if no mint provided
-//     let batch_mint_msg = ExecuteMsg::BatchMint { num_mints: 1 };
-//     let res = router.execute_contract(
-//         buyer.clone(),
-//         minter_addr.clone(),
-//         &batch_mint_msg,
-//         &coins(PRICE, NATIVE_DENOM),
-//     );
-//     assert!(res.is_ok());
+    // batch mint limit set to STARTING_BATCH_MINT_LIMIT if no mint provided
+    let batch_mint_msg = ExecuteMsg::BatchMint { num_mints: 1 };
+    let res = router.execute_contract(
+        buyer.clone(),
+        minter_addr.clone(),
+        &batch_mint_msg,
+        &coins(PRICE, NATIVE_DENOM),
+    );
+    assert!(res.is_ok());
 
-//     // update batch mint limit, test unauthorized
-//     let update_batch_mint_limit_msg = ExecuteMsg::UpdateBatchMintLimit {
-//         batch_mint_limit: 1,
-//     };
-//     let err = router
-//         .execute_contract(
-//             buyer.clone(),
-//             minter_addr.clone(),
-//             &update_batch_mint_limit_msg,
-//             &coins(PRICE, NATIVE_DENOM),
-//         )
-//         .unwrap_err();
-//     assert_eq!(
-//         err.source().unwrap().to_string(),
-//         ContractError::Unauthorized("Sender is not an admin".to_string()).to_string()
-//     );
+    // update batch mint limit, test unauthorized
+    let update_batch_mint_limit_msg = ExecuteMsg::UpdateBatchMintLimit {
+        batch_mint_limit: 1,
+    };
+    let err = router
+        .execute_contract(
+            buyer.clone(),
+            minter_addr.clone(),
+            &update_batch_mint_limit_msg,
+            &[],
+        )
+        .unwrap_err();
+    assert_eq!(
+        err.source().unwrap().to_string(),
+        ContractError::Unauthorized("Sender is not an admin".to_string()).to_string()
+    );
 
-//     // update limit, invalid limit over max
-//     let update_batch_mint_limit_msg = ExecuteMsg::UpdateBatchMintLimit {
-//         batch_mint_limit: 100,
-//     };
-//     let err = router
-//         .execute_contract(
-//             creator.clone(),
-//             minter_addr.clone(),
-//             &update_batch_mint_limit_msg,
-//             &coins(PRICE, NATIVE_DENOM),
-//         )
-//         .unwrap_err();
-//     assert_eq!(
-//         ContractError::InvalidBatchMintLimit {
-//             max: 30.to_string(),
-//             got: 100.to_string()
-//         }
-//         .to_string(),
-//         err.source().unwrap().to_string()
-//     );
+    // update limit, invalid limit over max
+    let update_batch_mint_limit_msg = ExecuteMsg::UpdateBatchMintLimit {
+        batch_mint_limit: 100,
+    };
+    let err = router
+        .execute_contract(
+            creator.clone(),
+            minter_addr.clone(),
+            &update_batch_mint_limit_msg,
+            &[],
+        )
+        .unwrap_err();
+    assert_eq!(
+        ContractError::InvalidBatchMintLimit {
+            max: 30.to_string(),
+            got: 100.to_string()
+        }
+        .to_string(),
+        err.source().unwrap().to_string()
+    );
 
-//     // update limit successfully as admin
-//     let update_batch_mint_limit_msg = ExecuteMsg::UpdateBatchMintLimit {
-//         batch_mint_limit: 2,
-//     };
-//     let res = router.execute_contract(
-//         creator.clone(),
-//         minter_addr.clone(),
-//         &update_batch_mint_limit_msg,
-//         &coins(PRICE, NATIVE_DENOM),
-//     );
-//     assert!(res.is_ok());
+    // update limit successfully as admin
+    let update_batch_mint_limit_msg = ExecuteMsg::UpdateBatchMintLimit {
+        batch_mint_limit: 2,
+    };
+    let res = router.execute_contract(
+        creator.clone(),
+        minter_addr.clone(),
+        &update_batch_mint_limit_msg,
+        &[],
+    );
+    assert!(res.is_ok());
 
-//     // test over max batch mint limit
-//     let batch_mint_msg = ExecuteMsg::BatchMint { num_mints: 50 };
-//     let err = router
-//         .execute_contract(
-//             buyer.clone(),
-//             minter_addr.clone(),
-//             &batch_mint_msg,
-//             &coins(PRICE, NATIVE_DENOM),
-//         )
-//         .unwrap_err();
-//     assert_eq!(
-//         ContractError::MaxBatchMintLimitExceeded {}.to_string(),
-//         err.source().unwrap().to_string()
-//     );
+    // test over max batch mint limit
+    let batch_mint_msg = ExecuteMsg::BatchMint { num_mints: 50 };
+    let err = router
+        .execute_contract(
+            buyer.clone(),
+            minter_addr.clone(),
+            &batch_mint_msg,
+            &coins(PRICE, NATIVE_DENOM),
+        )
+        .unwrap_err();
+    assert_eq!(
+        ContractError::MaxBatchMintLimitExceeded {}.to_string(),
+        err.source().unwrap().to_string()
+    );
 
-//     // success
-//     let num_mints = 2;
-//     let batch_mint_msg = ExecuteMsg::BatchMint { num_mints };
-//     let res = router.execute_contract(
-//         buyer.clone(),
-//         minter_addr.clone(),
-//         &batch_mint_msg,
-//         &coins(PRICE, NATIVE_DENOM),
-//     );
-//     assert!(res.is_ok());
+    // success
+    let num_mints = 2;
+    let batch_mint_msg = ExecuteMsg::BatchMint { num_mints };
+    let res = router.execute_contract(
+        buyer.clone(),
+        minter_addr.clone(),
+        &batch_mint_msg,
+        &coins(PRICE * (num_mints as u128), NATIVE_DENOM),
+    );
+    assert!(res.is_ok());
 
-//     // Balances are correct
-//     let buyer_native_balances = router.wrap().query_all_balances(buyer.clone()).unwrap();
-//     assert_eq!(
-//         buyer_native_balances,
-//         coins(INITIAL_BALANCE - (PRICE * num_mints as u128), NATIVE_DENOM)
-//     );
-//     // minter contract should have no balance
-//     let minter_balance = router
-//         .wrap()
-//         .query_all_balances(minter_addr.clone())
-//         .unwrap();
-//     assert_eq!(0, minter_balance.len());
+    // test sold out and fails
+    let num_mints = 2;
+    let batch_mint_msg = ExecuteMsg::BatchMint { num_mints };
+    let res = router.execute_contract(
+        buyer.clone(),
+        minter_addr.clone(),
+        &batch_mint_msg,
+        &coins(PRICE * num_mints as u128, NATIVE_DENOM),
+    );
+    assert!(res.is_err());
+    let err = res.unwrap_err();
+    if let Some(nested_err) = err.source() {
+        assert_eq!(
+            nested_err.source().unwrap().to_string(),
+            ContractError::SoldOut {}.to_string(),
+        );
+    };
 
-//     let creator_native_balances = router.wrap().query_all_balances(creator.clone()).unwrap();
-//     assert_eq!(
-//         creator_native_balances,
-//         coins(INITIAL_BALANCE + (PRICE * num_mints as u128), NATIVE_DENOM)
-//     );
-
-//     // test sold out and fails
-//     let num_mints = 2;
-//     let batch_mint_msg = ExecuteMsg::BatchMint { num_mints };
-//     let res = router.execute_contract(
-//         buyer.clone(),
-//         minter_addr.clone(),
-//         &batch_mint_msg,
-//         &coins(PRICE * num_mints as u128, NATIVE_DENOM),
-//     );
-//     assert!(res.is_err());
-//     let err = res.unwrap_err();
-//     if let Some(nested_err) = err.source() {
-//         assert_eq!(
-//             nested_err.source().unwrap().to_string(),
-//             ContractError::SoldOut {}.to_string(),
-//         );
-//     };
-
-//     // batch mint smaller amount
-//     let batch_mint_msg = ExecuteMsg::BatchMint { num_mints: 1 };
-//     let res = router.execute_contract(
-//         buyer,
-//         minter_addr,
-//         &batch_mint_msg,
-//         &coins(PRICE, NATIVE_DENOM),
-//     );
-//     assert!(res.is_ok());
-// }
+    // batch mint smaller amount
+    let batch_mint_msg = ExecuteMsg::BatchMint { num_mints: 1 };
+    let res = router.execute_contract(
+        buyer,
+        minter_addr,
+        &batch_mint_msg,
+        &coins(PRICE, NATIVE_DENOM),
+    );
+    assert!(res.is_ok());
+}
 
 #[test]
 fn mint_for_token_id_addr() {
