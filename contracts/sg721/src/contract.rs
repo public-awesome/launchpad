4588d241593bd26373ad792068f8356933dbfe23
Shane Vitarana <shanev@gmail.com>
2022-02-28 15:33:46 -0500

    Added query test

--- contracts/sg721/src/contract.rs
+++ contracts/sg721/src/contract.rs
@@ -220,5 +220,14 @@ mod tests {
             }),
             value.royalty
         );
+
+        let res = query(deps.as_ref(), mock_env(), QueryMsg::Config {}).unwrap();
+        let value: ConfigResponse = from_binary(&res).unwrap();
+        assert_eq!("ipfs://bafyreibvxty5gjyeedk7or7tahyrzgbrwjkolpairjap3bmegvcjdipt74.ipfs.dweb.link/metadata.json".to_string(), value.contract_uri.unwrap());
+        assert_eq!("creator", value.creator.unwrap().to_string());
+        assert_eq!(
+            "creator",
+            value.royalty.unwrap().payment_address.to_string()
+        );
     }
 }
