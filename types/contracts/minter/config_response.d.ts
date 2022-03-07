c55ea7fb4346215fb91d4ea6726a44f6141c6134
Shane Vitarana <shanev@gmail.com>
2022-03-06 22:31:00 -0500

    Update schema and types

--- types/contracts/minter/config_response.d.ts
+++ types/contracts/minter/config_response.d.ts
@@ -1,14 +1,14 @@
-import { Addr, Coin, Timestamp } from "./shared-types";
+import { Coin, Timestamp } from "./shared-types";
 
 export interface ConfigResponse {
-admin: Addr
+admin: string
 base_token_uri: string
 num_tokens: number
 per_address_limit: number
-sg721_address: Addr
+sg721_address: string
 sg721_code_id: number
 start_time: Timestamp
 unit_price: Coin
-whitelist?: (Addr | null)
+whitelist?: (string | null)
 [k: string]: unknown
 }
