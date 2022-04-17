4bd8ab611e979b39821a784d7d2be7ac5ef99dcf
Shane Vitarana <shanev@gmail.com>
2022-04-16 20:55:19 -0400

    Update naming

--- contracts/timelocked-stake/examples/schema.rs
+++ contracts/timelocked-stake/examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use sg_timelocked_stake::msg::{ExecuteMsg, InstantiateMsg, QueryMsg, StakeResponse};
+use timelocked_stake::msg::{ExecuteMsg, InstantiateMsg, QueryMsg, StakeResponse};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
