b4c7f7ffdb17ed3177b696dc3a52ec10f229331c
Dan Lynch <pyramation@gmail.com>
2022-04-17 12:22:19 -0700

    added missing schemas

--- contracts/sg721/examples/schema.rs
+++ contracts/sg721/examples/schema.rs
@@ -8,6 +8,9 @@ use cw721::{
     AllNftInfoResponse, ApprovalResponse, ApprovalsResponse, ContractInfoResponse, NftInfoResponse,
     NumTokensResponse, OperatorsResponse, OwnerOfResponse, TokensResponse,
 };
+use cw721_base::{
+    MinterResponse
+};
 use sg721::msg::{CollectionInfoResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
@@ -25,6 +28,17 @@ fn main() {
         &out_dir,
         "AllNftInfoResponse",
     );
+    export_schema_with_title(
+        &schema_for!(TokensResponse),
+        &out_dir,
+        "AllTokensResponse",
+    );
+    export_schema_with_title(
+        &schema_for!(OperatorsResponse),
+        &out_dir,
+        "AllOperatorsResponse",
+    );
+    export_schema(&schema_for!(MinterResponse), &out_dir);
     export_schema(&schema_for!(ApprovalResponse), &out_dir);
     export_schema(&schema_for!(ApprovalsResponse), &out_dir);
     export_schema(&schema_for!(OperatorsResponse), &out_dir);
