000f1783d248ec73591b0fafba68c781ef85e214
Tasio Victoria <tasiovictoria@gmail.com>
2022-04-22 15:09:31 -0400

    Add cw2981-royalties contract

--- contracts/cw2981-royalties/scripts/optimize.sh
+++ contracts/cw2981-royalties/scripts/optimize.sh
@@ -4,4 +4,4 @@ docker run --rm -v "$(pwd)":/code \
 	--platform linux/x86_64 \
 	--mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
 	--mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-	cosmwasm/workspace-optimizer:0.12.6
+	cosmwasm/rust-optimizer:0.12.6
