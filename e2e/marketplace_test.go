c268906b770b9eb15c7bbcdfce2c5be58aa4996e
Shane Vitarana <shanev@gmail.com>
2022-04-27 18:25:56 -0400

    Added minter

--- e2e/marketplace_test.go
+++ e2e/marketplace_test.go
@@ -14,12 +14,12 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/public-awesome/stargaze/v4/testutil/simapp"
 	"github.com/stretchr/testify/require"
+	"github.com/tendermint/tendermint/crypto/secp256k1"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 )
 
 func TestMarketplace(t *testing.T) {
 	accs := GetAccounts()
-	addr1 := accs[1].Address
 
 	genAccs, balances := GetAccountsAndBalances(accs)
 
@@ -35,18 +35,12 @@ func TestMarketplace(t *testing.T) {
 	wasmParams.MaxWasmCodeSize = 1000 * 1024 * 4 // 4MB
 	app.WasmKeeper.SetParams(ctx, wasmParams)
 
-	// download latest marketplace code
-	out, err := os.Create("contracts/sg_marketplace.wasm")
-	require.NoError(t, err)
-	defer out.Close()
-	resp, err := http.Get("https://github.com/public-awesome/marketplace/releases/latest/download/sg_marketplace.wasm")
-	require.NoError(t, err)
-	defer resp.Body.Close()
-	_, err = io.Copy(out, resp.Body)
-	require.NoError(t, err)
+	priv1 := secp256k1.GenPrivKey()
+	pub1 := priv1.PubKey()
+	addr1 := sdk.AccAddress(pub1.Address())
 
-	// marketplace
-	b, err := ioutil.ReadFile("contracts/sg_marketplace.wasm")
+	// minter
+	b, err := ioutil.ReadFile("contracts/minter.wasm")
 	require.NoError(t, err)
 
 	msgServer := wasmkeeper.NewMsgServerImpl(wasmkeeper.NewDefaultPermissionKeeper(app.WasmKeeper))
@@ -58,9 +52,82 @@ func TestMarketplace(t *testing.T) {
 	require.NotNil(t, res)
 	require.Equal(t, res.CodeID, uint64(1))
 
+	// whitelist
+	b, err = ioutil.ReadFile("contracts/whitelist.wasm")
+	require.NoError(t, err)
+
+	res, err = msgServer.StoreCode(sdk.WrapSDKContext(ctx), &wasmtypes.MsgStoreCode{
+		Sender:       addr1.String(),
+		WASMByteCode: b,
+	})
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	require.Equal(t, res.CodeID, uint64(2))
+
+	// sg721
+	b, err = ioutil.ReadFile("contracts/sg721.wasm")
+	require.NoError(t, err)
+
+	res, err = msgServer.StoreCode(sdk.WrapSDKContext(ctx), &wasmtypes.MsgStoreCode{
+		Sender:       addr1.String(),
+		WASMByteCode: b,
+	})
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	require.Equal(t, res.CodeID, uint64(3))
+
 	creator := accs[0]
+	// minter
+
+	genesisMintDateTime, err := time.Parse(time.RFC3339Nano, "2022-03-11T21:00:00Z")
+	require.NoError(t, err)
 
 	instantiateMsgRaw := []byte(
+		fmt.Sprintf(instantiateMinterTemplate,
+			creator.Address.String(),
+			creator.Address.String(),
+			creator.Address.String(),
+			genesisMintDateTime.UnixNano(),
+			"null",
+			1, // limit 1
+		),
+	)
+	instantiateRes, err := msgServer.InstantiateContract(sdk.WrapSDKContext(ctx), &wasmtypes.MsgInstantiateContract{
+		Sender: creator.Address.String(),
+		Admin:  creator.Address.String(),
+		CodeID: 1,
+		Label:  "Minter",
+		Msg:    instantiateMsgRaw,
+		Funds:  sdk.NewCoins(sdk.NewInt64Coin("ustars", 1_000_000_000)),
+	})
+	require.NoError(t, err)
+	require.NotNil(t, instantiateRes)
+	require.NotEmpty(t, instantiateRes.Address)
+	minterAddress := instantiateRes.Address
+
+	// download latest marketplace code
+	out, err := os.Create("contracts/sg_marketplace.wasm")
+	require.NoError(t, err)
+	defer out.Close()
+	resp, err := http.Get("https://github.com/public-awesome/marketplace/releases/latest/download/sg_marketplace.wasm")
+	require.NoError(t, err)
+	defer resp.Body.Close()
+	_, err = io.Copy(out, resp.Body)
+	require.NoError(t, err)
+
+	// marketplace
+	b, err = ioutil.ReadFile("contracts/sg_marketplace.wasm")
+	require.NoError(t, err)
+
+	res, err = msgServer.StoreCode(sdk.WrapSDKContext(ctx), &wasmtypes.MsgStoreCode{
+		Sender:       addr1.String(),
+		WASMByteCode: b,
+	})
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	require.Equal(t, res.CodeID, uint64(4))
+
+	instantiateMsgRaw = []byte(
 		fmt.Sprintf(instantiateMarketplaceTemplate,
 			2,
 			86400,
@@ -70,12 +137,11 @@ func TestMarketplace(t *testing.T) {
 			creator.Address.String(),
 		),
 	)
-
 	// instantiate marketplace
-	instantiateRes, err := msgServer.InstantiateContract(sdk.WrapSDKContext(ctx), &wasmtypes.MsgInstantiateContract{
+	instantiateRes, err = msgServer.InstantiateContract(sdk.WrapSDKContext(ctx), &wasmtypes.MsgInstantiateContract{
 		Sender: addr1.String(),
 		Admin:  addr1.String(),
-		CodeID: 1,
+		CodeID: 4,
 		Label:  "Marketplace",
 		Msg:    instantiateMsgRaw,
 	})
@@ -84,5 +150,19 @@ func TestMarketplace(t *testing.T) {
 	require.NotEmpty(t, instantiateRes.Address)
 	marketplaceAddress := instantiateRes.Address
 	require.NotEmpty(t, marketplaceAddress)
+
+	// go to the future
+	afterGenesisMint, err := time.Parse(time.RFC3339Nano, "2022-03-11T21:00:01Z")
+	require.NoError(t, err)
+	ctx = app.BaseApp.NewContext(false, tmproto.Header{Height: 1, ChainID: "stargaze-1", Time: afterGenesisMint})
+
+	// mint succeeds
+	_, err = msgServer.ExecuteContract(sdk.WrapSDKContext(ctx), &wasmtypes.MsgExecuteContract{
+		Contract: minterAddress,
+		Sender:   accs[1].Address.String(),
+		Msg:      []byte(`{"mint":{}}`),
+		Funds:    sdk.NewCoins(sdk.NewInt64Coin("ustars", 100_000_000)),
+	})
+	require.NoError(t, err)
 
 }
