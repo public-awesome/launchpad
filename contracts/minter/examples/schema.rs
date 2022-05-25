4089aebc7f743eb0af627e79c54b63c568712db1
John Y <j@publicawesome.com>
2022-05-25 16:47:29 -0400

    fix schema fmt

--- contracts/minter/examples/schema.rs
+++ contracts/minter/examples/schema.rs
@@ -4,14 +4,8 @@ use std::fs::create_dir_all;
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
 use minter::msg::{
-    ConfigResponse,
-    ExecuteMsg,
-    InstantiateMsg,
-    QueryMsg,
-    MintableNumTokensResponse,
-    MintCountResponse,
-    StartTimeResponse,
-    MintPriceResponse
+    ConfigResponse, ExecuteMsg, InstantiateMsg, MintCountResponse, MintPriceResponse,
+    MintableNumTokensResponse, QueryMsg, StartTimeResponse,
 };
 use minter::state::Config;
 
