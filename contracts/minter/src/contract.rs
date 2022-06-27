20c2c16a1d6b34d9086b9bb4f8a7b36e3f668d06
John Y <j@publicawesome.com>
2022-06-27 19:09:42 -0400

    Revert "remove shuffle on instantiate"
    This reverts commit 2dff8f3d7af7881a1bbfc4fb23f2b716473558b4.

--- contracts/minter/src/contract.rs
+++ contracts/minter/src/contract.rs
@@ -124,9 +124,12 @@ pub fn instantiate(
     CONFIG.save(deps.storage, &config)?;
     MINTABLE_NUM_TOKENS.save(deps.storage, &msg.num_tokens)?;
 
+    let token_list = random_token_list(&env, msg.num_tokens)?;
     // Save mintable token ids map
-    for token_id in 1..=config.num_tokens {
-        MINTABLE_TOKEN_POSITIONS.save(deps.storage, token_id, &token_id)?;
+    let mut token_position: u32 = 1;
+    for token_id in token_list {
+        MINTABLE_TOKEN_POSITIONS.save(deps.storage, token_position, &token_id)?;
+        token_position += 1;
     }
 
     // Submessage to instantiate sg721 contract
