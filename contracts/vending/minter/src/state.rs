bd682ef1cb23238cb91dfda0eae30d02017338e3
Shane Vitarana <shanev@gmail.com>
2022-07-19 00:38:24 -0400

    Renamed vending minter

---
+++ contracts/vending/minter/src/state.rs
@@ -0,0 +1,24 @@
+use cosmwasm_std::{Addr, Coin, Timestamp};
+use cw_storage_plus::{Item, Map};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Config {
+    pub admin: Addr,
+    pub factory: Addr,
+    pub base_token_uri: String,
+    pub num_tokens: u32,
+    pub sg721_code_id: u64,
+    pub unit_price: Coin,
+    pub whitelist: Option<Addr>,
+    pub start_time: Timestamp,
+    pub per_address_limit: u32,
+}
+
+pub const CONFIG: Item<Config> = Item::new("config");
+pub const SG721_ADDRESS: Item<Addr> = Item::new("sg721_address");
+// map of index position and token id
+pub const MINTABLE_TOKEN_POSITIONS: Map<u32, u32> = Map::new("mt");
+pub const MINTABLE_NUM_TOKENS: Item<u32> = Item::new("mintable_num_tokens");
+pub const MINTER_ADDRS: Map<Addr, u32> = Map::new("ma");
