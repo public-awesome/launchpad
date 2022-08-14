a761ff6d9ddc0f9ec2c774522b7ecfb5253abaf3
Shane Vitarana <shanev@gmail.com>
2022-08-14 23:45:30 +0900

    Distribute funds

--- contracts/splits/src/contract.rs
+++ contracts/splits/src/contract.rs
@@ -1,15 +1,18 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cosmwasm_std::{
+    coin, to_binary, BankMsg, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, Uint128,
+};
 use cw2::set_contract_version;
-use cw4::Cw4Contract;
+use cw4::{Cw4Contract, Member, MemberListResponse, MemberResponse};
+use sg_std::NATIVE_DENOM;
 
 use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 use crate::state::{Config, CONFIG};
 
 // version info for migration info
-const CONTRACT_NAME: &str = "crates.io:splits";
+const CONTRACT_NAME: &str = "crates.io:sg-splits";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -24,63 +27,95 @@ pub fn instantiate(
             addr: msg.group_addr.clone(),
         }
     })?);
+    // TODO: validate that this is a group contract by querying something
 
-    let state = State {
-        count: msg.count,
-        owner: info.sender.clone(),
-    };
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
-    STATE.save(deps.storage, &state)?;
 
-    Ok(Response::new()
-        .add_attribute("method", "instantiate")
-        .add_attribute("owner", info.sender)
-        .add_attribute("count", msg.count.to_string()))
+    let cfg = Config {
+        group_addr,
+        executor: msg.executor,
+    };
+    CONFIG.save(deps.storage, &cfg)?;
+
+    Ok(Response::default())
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
     deps: DepsMut,
-    _env: Env,
+    env: Env,
     info: MessageInfo,
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
     match msg {
-        ExecuteMsg::Increment {} => try_increment(deps),
-        ExecuteMsg::Reset { count } => try_reset(deps, info, count),
+        ExecuteMsg::Distribute {} => todo!(),
     }
 }
 
-pub fn try_increment(deps: DepsMut) -> Result<Response, ContractError> {
-    STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
-        state.count += 1;
-        Ok(state)
-    })?;
+pub fn execute_distribute(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+) -> Result<Response, ContractError> {
+    let config = CONFIG.load(deps.storage)?;
+    let total_weight = config.group_addr.total_weight(&deps.querier)?;
+    let members = config.group_addr.list_members(&deps.querier, None, None)?;
 
-    Ok(Response::new().add_attribute("method", "try_increment"))
-}
+    let funds = deps
+        .querier
+        .query_balance(env.contract.address, NATIVE_DENOM)?;
 
-pub fn try_reset(deps: DepsMut, info: MessageInfo, count: i32) -> Result<Response, ContractError> {
-    STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
-        if info.sender != state.owner {
-            return Err(ContractError::Unauthorized {});
-        }
-        state.count = count;
-        Ok(state)
-    })?;
-    Ok(Response::new().add_attribute("method", "reset"))
+    let msgs = members
+        .iter()
+        .map(|member| {
+            let ratio = Uint128::from(member.weight) / Uint128::from(total_weight);
+            let member_amount = funds.amount * ratio;
+            BankMsg::Send {
+                to_address: member.addr,
+                amount: vec![coin(member_amount.u128(), funds.denom)],
+            }
+        })
+        .collect::<Vec<_>>();
+
+    Ok(Response::new()
+        .add_attribute("action", "distribute")
+        .add_messages(msgs))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
-        QueryMsg::GetCount {} => to_binary(&query_count(deps)?),
+        QueryMsg::ListMembers { start_after, limit } => {
+            to_binary(&list_members(deps, start_after, limit)?)
+        }
+        QueryMsg::Member { address } => to_binary(&query_member(deps, address)?),
     }
 }
 
-fn query_count(deps: Deps) -> StdResult<GetCountResponse> {
-    let state = STATE.load(deps.storage)?;
-    Ok(GetCountResponse { count: state.count })
+fn query_member(deps: Deps, member: String) -> StdResult<MemberResponse> {
+    let cfg = CONFIG.load(deps.storage)?;
+    let voter_addr = deps.api.addr_validate(&member)?;
+    let weight = cfg.group_addr.is_member(&deps.querier, &voter_addr, None)?;
+
+    Ok(MemberResponse { weight })
+}
+
+fn list_members(
+    deps: Deps,
+    start_after: Option<String>,
+    limit: Option<u32>,
+) -> StdResult<MemberListResponse> {
+    let cfg = CONFIG.load(deps.storage)?;
+    let members = cfg
+        .group_addr
+        .list_members(&deps.querier, start_after, limit)?
+        .into_iter()
+        .map(|member| Member {
+            addr: member.addr,
+            weight: member.weight,
+        })
+        .collect();
+    Ok(MemberListResponse { members })
 }
 
 #[cfg(test)]
