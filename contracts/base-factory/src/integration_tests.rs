6b697a685a9d1d9dde7cf4e4270ebaff44528300
Shane Vitarana <shanev@gmail.com>
2022-08-20 17:28:30 -0400

    Fixed config toml

--- contracts/base-factory/src/integration_tests.rs
+++ contracts/base-factory/src/integration_tests.rs
@@ -30,7 +30,7 @@ mod tests {
 
     pub fn mock_params() -> BaseMinterParams {
         BaseMinterParams {
-            code_id: 1,
+            code_id: 2,
             creation_fee: coin(CREATION_FEE, NATIVE_DENOM),
             min_mint_price: coin(MIN_MINT_PRICE, NATIVE_DENOM),
             mint_fee_bps: MINT_FEE_BPS,
