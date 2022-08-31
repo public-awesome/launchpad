7931ba40d01501e38dce89e674f43e51666944e0
John Y <j@publicawesome.com>
2022-08-31 13:21:24 -0400

    fix linter

--- contracts/vending-minter/src/contract.rs
+++ contracts/vending-minter/src/contract.rs
@@ -190,15 +190,13 @@ fn get_start_trading_time(
                 start_trading_time,
                 start_time,
             ));
+        } else if start_trading_time < latest_trading_start_time {
+            start_trading_time
         } else {
-            if start_trading_time < latest_trading_start_time {
-                start_trading_time
-            } else {
-                return Err(ContractError::InvalidStartTradingTime(
-                    latest_trading_start_time,
-                    start_trading_time,
-                ));
-            }
+            return Err(ContractError::InvalidStartTradingTime(
+                latest_trading_start_time,
+                start_trading_time,
+            ));
         }
     } else {
         latest_trading_start_time
