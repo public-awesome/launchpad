d078658a909d9f69d7b0752ab3a0a0bf89dc313a
Humanalgorithm <scottomj@gmail.com>
2022-11-12 19:31:56 -0500

    fix wrong name

--- contracts/sg-eth-airdrop/examples/schema.rs
+++ contracts/sg-eth-airdrop/examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use sg_eth_aidrop::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use sg_eth_airdrop::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
