4ebdb8f97431bebbfa73c3c0f89481a77b003303
John Y <j@publicawesome.com>
2022-11-24 08:40:42 -0500

    refactor

--- contracts/vending-minter/src/contract.rs
+++ contracts/vending-minter/src/contract.rs
@@ -83,16 +83,13 @@ pub fn instantiate(
     }
 
     // sanitize base token uri
-    let base_token_uri = msg
-        .init_msg
-        .base_token_uri
-        .trim()
-        .replacen("IPFS", "ipfs", 1);
+    let mut base_token_uri = msg.init_msg.base_token_uri.trim().to_string();
     // Check that base_token_uri is a valid IPFS uri
     let parsed_token_uri = Url::parse(&base_token_uri)?;
     if parsed_token_uri.scheme() != "ipfs" {
         return Err(ContractError::InvalidBaseTokenURI {});
     }
+    base_token_uri = parsed_token_uri.to_string();
 
     let genesis_time = Timestamp::from_nanos(GENESIS_MINT_START_TIME);
     // If start time is before genesis time return error
