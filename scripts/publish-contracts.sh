8d143489f4a4f7b9467b8a65b483451e2339e81f
Shane Vitarana <shanev@gmail.com>
2023-01-30 17:46:16 -0500

    Added back sleeps

--- scripts/publish-contracts.sh
+++ scripts/publish-contracts.sh
@@ -1,10 +1,19 @@
-cd contracts/base-factory && cargo publish && cd ../..
-cd contracts/sg721-base && cargo publish && cd ../..
+# cd contracts/base-factory && cargo publish && cd ../..
+# sleep 10
+# cd contracts/sg721-base && cargo publish && cd ../..
+# sleep 10
 cd contracts/sg721-metadata-onchain && cargo publish && cd ../..
+sleep 10
 cd contracts/sg721-nt && cargo publish && cd ../..
+sleep 10
 cd contracts/splits && cargo publish && cd ../..
+sleep 10
 cd contracts/base-minter && cargo publish && cd ../..
+sleep 10
 cd contracts/whitelist && cargo publish && cd ../..
+sleep 10
 cd contracts/vending-factory && cargo publish && cd ../..
+sleep 10
 cd contracts/vending-minter && cargo publish && cd ../..
+sleep 10
 cd contracts/whitelist-immutable && cargo publish && cd ../..
--- scripts/publish-packages.sh
+++ scripts/publish-packages.sh
@@ -1,10 +1,19 @@
 cd packages/sg-std && cargo publish && cd ../..
+sleep 10
 cd packages/sg721 && cargo publish && cd ../..
+sleep 10
 cd packages/sg-metadata && cargo publish && cd ../..
+sleep 10
 cd packages/sg4 && cargo publish && cd ../..
+sleep 10
 cd packages/sg2 && cargo publish && cd ../..
+sleep 10
 cd packages/sg-multi-test && cargo publish && cd ../..
+sleep 10
 cd packages/sg-utils && cargo publish && cd ../..
+sleep 10
 cd packages/sg1 && cargo publish && cd ../..
+sleep 10
 cd packages/controllers && cargo publish && cd ../..
+sleep 10
 cd packages/ethereum-verify && cargo publish && cd ../..
