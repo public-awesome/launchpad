ab312183f44b09416912b134137d4de410110d1b
Shane Vitarana <shanev@gmail.com>
2023-01-31 00:00:10 -0500

    Update base minter schema

---
+++ contracts/base-minter/src/bin/schema.rs
@@ -0,0 +1,10 @@
+use cosmwasm_schema::write_api;
+
+use base_minter::msg::{ExecuteMsg, InstantiateMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+    }
+}
