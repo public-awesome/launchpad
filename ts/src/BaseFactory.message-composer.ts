dcc912324ca042ef7abbdce480a69bc09e22fa9d
Shane Vitarana <shanev@gmail.com>
2023-02-01 16:13:16 -0500

    Update TS types

--- ts/src/BaseFactory.message-composer.ts
+++ ts/src/BaseFactory.message-composer.ts
@@ -4,4 +4,51 @@
 * and run the @cosmwasm/ts-codegen generate command to regenerate this file.
 */
 
-import { Uint128, InstantiateMsg, MinterParamsForNullable_Empty, Coin, Empty, Sg2QueryMsg } from "./BaseFactory.types";
+import { MsgExecuteContractEncodeObject } from "cosmwasm";
+import { MsgExecuteContract } from "cosmjs-types/cosmwasm/wasm/v1/tx";
+import { toUtf8 } from "@cosmjs/encoding";
+import { Uint128, InstantiateMsg, MinterParamsForNullable_Empty, Coin, Empty, ExecuteMsg, Decimal, Timestamp, Uint64, CreateMinterMsgForNullable_Empty, CollectionParams, CollectionInfoForRoyaltyInfoResponse, RoyaltyInfoResponse } from "./BaseFactory.types";
+export interface BaseFactoryMessage {
+  contractAddress: string;
+  sender: string;
+  createMinter: ({
+    collectionParams,
+    initMsg
+  }: {
+    collectionParams: CollectionParams;
+    initMsg?: Empty;
+  }, funds?: Coin[]) => MsgExecuteContractEncodeObject;
+}
+export class BaseFactoryMessageComposer implements BaseFactoryMessage {
+  sender: string;
+  contractAddress: string;
+
+  constructor(sender: string, contractAddress: string) {
+    this.sender = sender;
+    this.contractAddress = contractAddress;
+    this.createMinter = this.createMinter.bind(this);
+  }
+
+  createMinter = ({
+    collectionParams,
+    initMsg
+  }: {
+    collectionParams: CollectionParams;
+    initMsg?: Empty;
+  }, funds?: Coin[]): MsgExecuteContractEncodeObject => {
+    return {
+      typeUrl: "/cosmwasm.wasm.v1.MsgExecuteContract",
+      value: MsgExecuteContract.fromPartial({
+        sender: this.sender,
+        contract: this.contractAddress,
+        msg: toUtf8(JSON.stringify({
+          create_minter: {
+            collection_params: collectionParams,
+            init_msg: initMsg
+          }
+        })),
+        funds
+      })
+    };
+  };
+}
