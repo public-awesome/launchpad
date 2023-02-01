4b05f003f388708f434ab24e60ad8056cec41a0e
Shane Vitarana <shanev@gmail.com>
2023-02-01 17:49:56 -0500

    Update scripts

--- scripts/publish-contracts.sh
+++ scripts/publish-contracts.sh
@@ -1,7 +1,7 @@
 cd contracts/base-factory && cargo publish && cd ../..
 sleep 10
 cd contracts/sg721-base && cargo publish && cd ../..
-sleep 10
+sleep 15
 cd contracts/sg721-metadata-onchain && cargo publish && cd ../..
 sleep 10
 cd contracts/sg721-nt && cargo publish && cd ../..
@@ -13,7 +13,7 @@ sleep 10
 cd contracts/whitelist && cargo publish && cd ../..
 sleep 10
 cd contracts/vending-factory && cargo publish && cd ../..
-sleep 10
+sleep 15
 cd contracts/vending-minter && cargo publish && cd ../..
 sleep 10
 cd contracts/whitelist-immutable && cargo publish && cd ../..
