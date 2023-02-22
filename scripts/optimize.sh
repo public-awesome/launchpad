e56da873f1da6b7b077a6c0f46ac9e15ed64155b
Shane Vitarana <shanev@gmail.com>
2023-02-22 13:34:55 -0500

    Update optimizer version

--- scripts/optimize.sh
+++ scripts/optimize.sh
@@ -1,4 +1,4 @@
-docker run --rm -v "$(pwd)":/code --platform linux/amd64 \
-	--mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
-	--mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-	cosmwasm/workspace-optimizer:0.12.11
+docker run --rm -v "$(pwd)":/code \
+    --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
+    --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+    cosmwasm/workspace-optimizer-arm64:0.12.11
