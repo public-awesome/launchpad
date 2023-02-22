922543072dfe18ec60233ae361261b9c7a121e7a
Shane Vitarana <shanev@gmail.com>
2023-02-22 13:42:45 -0500

    Update upload script

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,20 +1,3 @@
-# echo "base-factory";
-# starsd tx wasm store artifacts/base_factory.wasm --from test --gas-prices 0.025ustars --gas-adjustment 1.7 --gas auto --chain-id elgafar-1 --node https://rpc.elgafar-1.stargaze-apis.com:443  -b block --yes -o json | jq '.logs' | grep -A 1 code_id
-# echo "base-minter";
-# starsd tx wasm store artifacts/base_minter.wasm --from test --gas-prices 0.025ustars --gas-adjustment 1.7 --gas auto --chain-id elgafar-1 --node https://rpc.elgafar-1.stargaze-apis.com:443   -b block --yes -o json | jq '.logs' | grep -A 1 code_id
-
-# echo "vending-factory";
-# starsd tx wasm store artifacts/vending_factory.wasm --from test --gas-prices 0.025ustars --gas-adjustment 1.7 --gas auto --chain-id elgafar-1 --node https://rpc.elgafar-1.stargaze-apis.com:443  -b block --yes -o json | jq '.logs' | grep -A 1 code_id
-# echo "vending-sg721-base";
-# starsd tx wasm store artifacts/sg721_base.wasm --from test --gas-prices 0.025ustars --gas-adjustment 1.7 --gas auto --chain-id elgafar-1 --node https://rpc.elgafar-1.stargaze-apis.com:443 -b block --yes -o json | jq '.logs' | grep -A 1 code_id
-# echo "vending-minter";
-# starsd tx wasm store artifacts/vending_minter.wasm --from test --gas-prices 0.025ustars --gas-adjustment 1.7 --gas auto --chain-id elgafar-1 --node https://rpc.elgafar-1.stargaze-apis.com:443   -b block --yes -o json | jq '.logs' | grep -A 1 code_id
-# echo "base-factory";
-# starsd tx wasm store artifacts/base_factory.wasm --from test --gas-prices 0.025ustars --gas-adjustment 1.7 --gas auto --chain-id elgafar-1 --node https://rpc.elgafar-1.stargaze-apis.com:443   -b block --yes -o json | jq '.logs' | grep -A 1 code_id
-# echo "base-minter";
-# starsd tx wasm store artifacts/base_minter.wasm --from test --gas-prices 0.025ustars --gas-adjustment 1.7 --gas auto --chain-id elgafar-1 --node https://rpc.elgafar-1.stargaze-apis.com:443   -b block --yes -o json | jq '.logs' | grep -A 1 code_id
-
-
 for d in artifacts/*.wasm; do
     echo $d;
     starsd tx wasm store $d --from test --gas-prices 0.025ustars --gas-adjustment 1.7 --gas auto --chain-id elgafar-1 --node https://rpc.elgafar-1.stargaze-apis.com:443  -b block --yes -o json | jq '.logs' | grep -A 1 code_id
