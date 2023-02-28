603ed3e0717a12a99ce95a1ac6ea87420ef4014a
jhernandezb <contact@jhernandez.me>
2023-02-28 10:50:26 -0600

    update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-git branch release-$1
+git checkout -b release-$1
 
 version=$(grep "version" ./Cargo.toml | head -1 | cut -d '"' -f 2)
 echo "Current version: $version"
@@ -20,12 +19,10 @@ echo "Publishing contracts"
 echo "Generating schema"
 make schema
 
-# hack to fix schema generation
-rm -rf contracts/base-factory/schema/raw/ contracts/base-minter/schema/raw/ contracts/sg-eth-airdrop/schema/raw/ contracts/splits/schema/raw/ contracts/whitelist-immutable/schema/raw/
+git commit -am "Release $1"
+git push origin release-$1
 
 echo "Push tag to Github"
 git tag -a v$1 -m "Release $1"
 git push origin v$1
 
-git commit -am "Release $1"
-git push origin release-$1
