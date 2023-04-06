d23827d55ff558312a0bfd024f560f9557830d94
John Y <j@publicawesome.com>
2023-04-06 10:27:11 -0400

    add tests

---
+++ test-suite/src/vending_minter/tests/updatable.rs
@@ -0,0 +1,16 @@
+#[test]
+fn update_token_metadata() {
+    // create updatable collection and minter
+    // mint token
+    // update token metadata
+    // check token metadata
+}
+
+#[test]
+fn migrate_to_updatable() {
+    // create non-updatable collection and minter
+    // mint token
+    // migrate to updatable
+    // update token metadata
+    // check token metadata
+}
