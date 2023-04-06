5ca19244dec48c6063958d0d4cba3889efb49058
John Y <j@publicawesome.com>
2023-04-06 17:00:57 -0400

    check earliest upgradable version

--- contracts/collections/sg721-updatable/src/contract.rs
+++ contracts/collections/sg721-updatable/src/contract.rs
@@ -18,6 +18,7 @@ use sg_std::Response;
 use semver::Version;
 
 const COMPATIBLE_MIGRATION_CONTRACT_NAME: &str = "crates.io:sg721-base";
+const EARLIEST_COMPATIBLE_CONTRACT_VERSION: &str = "0.24.0";
 const CONTRACT_NAME: &str = "crates.io:sg721-updatable";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
@@ -112,7 +113,14 @@ pub fn _migrate(deps: DepsMut, _env: Env, _msg: Empty) -> Result<Response, Contr
     let new_version: Version = CONTRACT_VERSION
         .parse()
         .map_err(|_| StdError::generic_err("Invalid contract version"))?;
+    let earliest_version: Version = EARLIEST_COMPATIBLE_CONTRACT_VERSION
+        .parse()
+        .map_err(|_| StdError::generic_err("Invalid contract version"))?;
 
+    // current version not launchpad v2
+    if version < earliest_version {
+        return Err(StdError::generic_err("Cannot upgrade to a previous contract version").into());
+    }
     if version > new_version {
         return Err(StdError::generic_err("Cannot upgrade to a previous contract version").into());
     }
