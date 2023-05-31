fecac2070ea006334f981e3826261b1828202b23
Shane Vitarana <shanev@gmail.com>
2023-05-30 23:24:15 -0400

    Update factory types

--- packages/sg2/src/lib.rs
+++ packages/sg2/src/lib.rs
@@ -18,5 +18,7 @@ pub struct MinterParams<T> {
     pub min_mint_price: Coin,
     pub mint_fee_bps: u64,
     pub max_trading_offset_secs: u64,
+    pub max_royalty_bps: u64,
+    pub max_royalty_increase_rate_bps: u64,
     pub extension: T,
 }
