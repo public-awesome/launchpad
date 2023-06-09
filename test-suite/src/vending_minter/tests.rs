b224ed4778bb084d691362d6cd06897234757481
John Y <j@publicawesome.com>
2023-06-09 12:37:15 -0400

    add test cases

--- test-suite/src/vending_minter/tests.rs
+++ test-suite/src/vending_minter/tests.rs
@@ -3,6 +3,7 @@ mod allowed_code_ids;
 mod frozen_factory;
 mod happy_unhappy;
 mod ibc_asset_mint;
+mod increase_royalties;
 mod mint_and_burn;
 mod splits;
 mod trading_time;
---
+++ test-suite/src/vending_minter/tests/increase_royalties.rs
@@ -0,0 +1,11 @@
+#[test]
+fn happy_path() {}
+
+#[test]
+fn royalty_too_high() {}
+
+#[test]
+fn increase_royalty_too_soon() {}
+
+#[test]
+fn increase_royalty_too_much() {}
