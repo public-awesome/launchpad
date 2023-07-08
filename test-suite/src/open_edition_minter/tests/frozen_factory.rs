e61873fb84cd08c88a2328ab9f638bfc1a553511
Humanalgorithm <scottomj@gmail.com>
2023-07-08 18:13:21 -0400

    working frozen factory and update mint price

--- test-suite/src/open_edition_minter/tests/frozen_factory.rs
+++ test-suite/src/open_edition_minter/tests/frozen_factory.rs
@@ -1,86 +1,101 @@
-// use cosmwasm_std::{coins, Coin, Timestamp, Uint128};
-// use cw_multi_test::{BankSudo, Executor, SudoMsg};
-// use sg_std::{GENESIS_MINT_START_TIME, NATIVE_DENOM};
+use cosmwasm_std::{coins, Coin, Timestamp, Uint128};
+use cw_multi_test::{BankSudo, Executor, SudoMsg};
+use open_edition_factory::state::ParamsExtension;
+use sg_std::{GENESIS_MINT_START_TIME, NATIVE_DENOM};
 
-// use open_edition_factory::msg::{OpenEditionUpdateParamsExtension, OpenEditionUpdateParamsMsg};
-// use open_edition_factory::types::{NftData, NftMetadataType};
-// use sg2::msg::Sg2ExecuteMsg;
-// use sg2::tests::mock_collection_params_1;
+use open_edition_factory::msg::{OpenEditionUpdateParamsExtension, OpenEditionUpdateParamsMsg};
+use open_edition_factory::types::{NftData, NftMetadataType};
+use sg2::msg::Sg2ExecuteMsg;
+use sg2::tests::mock_collection_params_1;
 
-// use crate::common_setup::setup_minter::common::constants::CREATION_FEE;
-// use crate::common_setup::setup_minter::common::constants::MIN_MINT_PRICE_OPEN_EDITION;
-// use crate::common_setup::setup_minter::open_edition_minter::mock_params::mock_create_minter;
-// use crate::common_setup::templates::open_edition_minter_custom_template;
+use crate::common_setup::setup_minter::common::constants::MIN_MINT_PRICE_OPEN_EDITION;
+use crate::common_setup::setup_minter::common::constants::{CREATION_FEE, DEV_ADDRESS};
+use crate::common_setup::setup_minter::open_edition_minter::minter_params::{
+    default_nft_data, init_msg,
+};
+use crate::common_setup::setup_minter::open_edition_minter::mock_params::mock_create_minter;
+use crate::common_setup::templates::open_edition_minter_custom_template;
 
-// #[test]
-// fn frozen_factory_cannot_create_new_minters() {
-//     let vt = open_edition_minter_custom_template(None, None, None, None, None, None, None, None)
-//         .unwrap();
-//     let (mut router, creator, _buyer) = (vt.router, vt.accts.creator, vt.accts.buyer);
-//     let _minter_addr = vt.collection_response_vec[0].minter.clone().unwrap();
-//     let factory_addr = vt.collection_response_vec[0].factory.clone().unwrap();
+#[test]
+fn frozen_factory_cannot_create_new_minters() {
+    let params_extension = ParamsExtension {
+        max_per_address_limit: 10,
+        airdrop_mint_fee_bps: 100,
+        airdrop_mint_price: Coin {
+            denom: NATIVE_DENOM.to_string(),
+            amount: Uint128::new(100_000_000u128),
+        },
+        dev_fee_address: DEV_ADDRESS.to_string(),
+    };
+    let init_msg = init_msg(default_nft_data(), None, None, None, None);
+    let vt = open_edition_minter_custom_template(params_extension, init_msg).unwrap();
 
-//     let update_msg = OpenEditionUpdateParamsMsg {
-//         add_sg721_code_ids: None,
-//         rm_sg721_code_ids: None,
-//         frozen: Some(true),
-//         code_id: None,
-//         creation_fee: None,
-//         min_mint_price: None,
-//         mint_fee_bps: None,
-//         max_trading_offset_secs: None,
-//         extension: OpenEditionUpdateParamsExtension {
-//             max_per_address_limit: None,
-//             min_mint_price: None,
-//             airdrop_mint_fee_bps: None,
-//             airdrop_mint_price: None,
-//             dev_fee_address: None,
-//         },
-//     };
-//     let sudo_msg = open_edition_factory::msg::SudoMsg::UpdateParams(Box::new(update_msg));
-//     let _res = router.wasm_sudo(factory_addr.clone(), &sudo_msg);
+    let (mut router, creator, _buyer) = (vt.router, vt.accts.creator, vt.accts.buyer);
+    let _minter_addr = vt.collection_response_vec[0].minter.clone().unwrap();
+    let factory_addr = vt.collection_response_vec[0].factory.clone().unwrap();
 
-//     // Creating a new one should error
-//     let start_time = Timestamp::from_nanos(GENESIS_MINT_START_TIME + 100);
-//     let end_time = Timestamp::from_nanos(GENESIS_MINT_START_TIME + 10_000);
-//     let per_address_limit_minter = Some(1);
-//     let mint_price = Some(Coin {
-//         denom: NATIVE_DENOM.to_string(),
-//         amount: Uint128::new(MIN_MINT_PRICE_OPEN_EDITION),
-//     });
-//     let collection_params = mock_collection_params_1(Some(start_time));
-//     let default_nft_data = NftData {
-//         nft_data_type: NftMetadataType::OffChainMetadata,
-//         extension: None,
-//         token_uri: Some(
-//             "ipfs://bafybeiavall5udkxkdtdm4djezoxrmfc6o5fn2ug3ymrlvibvwmwydgrkm/1.jpg".to_string(),
-//         ),
-//     };
-//     let mut msg = mock_create_minter(
-//         Some(start_time),
-//         Some(end_time),
-//         mint_price,
-//         per_address_limit_minter,
-//         default_nft_data,
-//         collection_params,
-//         None,
-//     );
-//     msg.collection_params.code_id = 3;
-//     msg.collection_params.info.creator = creator.to_string();
-//     let creation_fee = coins(CREATION_FEE, NATIVE_DENOM);
-//     let msg = Sg2ExecuteMsg::CreateMinter(msg);
-//     router
-//         .sudo(SudoMsg::Bank({
-//             BankSudo::Mint {
-//                 to_address: creator.to_string(),
-//                 amount: coins(CREATION_FEE, NATIVE_DENOM),
-//             }
-//         }))
-//         .map_err(|err| println!("{:?}", err))
-//         .ok();
-//     let res = router.execute_contract(creator, factory_addr, &msg, &creation_fee);
-//     assert_eq!(
-//         res.err().unwrap().source().unwrap().to_string(),
-//         "Factory frozen. Cannot make new minters."
-//     );
-// }
+    let update_msg = OpenEditionUpdateParamsMsg {
+        add_sg721_code_ids: None,
+        rm_sg721_code_ids: None,
+        frozen: Some(true),
+        code_id: None,
+        creation_fee: None,
+        min_mint_price: None,
+        mint_fee_bps: None,
+        max_trading_offset_secs: None,
+        extension: OpenEditionUpdateParamsExtension {
+            max_per_address_limit: None,
+            min_mint_price: None,
+            airdrop_mint_fee_bps: None,
+            airdrop_mint_price: None,
+            dev_fee_address: None,
+        },
+    };
+
+    let sudo_msg = open_edition_factory::msg::SudoMsg::UpdateParams(Box::new(update_msg));
+    let _res = router.wasm_sudo(factory_addr.clone(), &sudo_msg);
+
+    // Creating a new one should error
+    let start_time = Timestamp::from_nanos(GENESIS_MINT_START_TIME + 100);
+    let end_time = Timestamp::from_nanos(GENESIS_MINT_START_TIME + 10_000);
+    let per_address_limit_minter = Some(1);
+    let mint_price = Some(Coin {
+        denom: NATIVE_DENOM.to_string(),
+        amount: Uint128::new(MIN_MINT_PRICE_OPEN_EDITION),
+    });
+    let collection_params = mock_collection_params_1(Some(start_time));
+    let default_nft_data = NftData {
+        nft_data_type: NftMetadataType::OffChainMetadata,
+        extension: None,
+        token_uri: Some(
+            "ipfs://bafybeiavall5udkxkdtdm4djezoxrmfc6o5fn2ug3ymrlvibvwmwydgrkm/1.jpg".to_string(),
+        ),
+    };
+    let mut msg = mock_create_minter(
+        Some(start_time),
+        Some(end_time),
+        mint_price,
+        per_address_limit_minter,
+        default_nft_data,
+        collection_params,
+        None,
+    );
+    msg.collection_params.code_id = 3;
+    msg.collection_params.info.creator = creator.to_string();
+    let creation_fee = coins(CREATION_FEE, NATIVE_DENOM);
+    let msg = Sg2ExecuteMsg::CreateMinter(msg);
+    router
+        .sudo(SudoMsg::Bank({
+            BankSudo::Mint {
+                to_address: creator.to_string(),
+                amount: coins(CREATION_FEE, NATIVE_DENOM),
+            }
+        }))
+        .map_err(|err| println!("{:?}", err))
+        .ok();
+    let res = router.execute_contract(creator, factory_addr, &msg, &creation_fee);
+    assert_eq!(
+        res.err().unwrap().source().unwrap().to_string(),
+        "Factory frozen. Cannot make new minters."
+    );
+}
--- test-suite/src/open_edition_minter/tests/update_mint_price.rs
+++ test-suite/src/open_edition_minter/tests/update_mint_price.rs
@@ -1,147 +1,169 @@
-// use cosmwasm_std::{Coin, Uint128};
-// use cw_multi_test::Executor;
-// use sg_std::{GENESIS_MINT_START_TIME, NATIVE_DENOM};
+use cosmwasm_std::{Coin, Uint128};
+use cw_multi_test::Executor;
+use open_edition_factory::state::ParamsExtension;
+use sg_std::{GENESIS_MINT_START_TIME, NATIVE_DENOM};
 
-// use open_edition_minter::msg::{ExecuteMsg, QueryMsg};
+use open_edition_minter::msg::{ExecuteMsg, QueryMsg};
 
-// use crate::common_setup::setup_accounts_and_block::setup_block_time;
-// use crate::common_setup::templates::open_edition_minter_custom_template;
+use crate::common_setup::setup_accounts_and_block::setup_block_time;
+use crate::common_setup::setup_minter::common::constants::DEV_ADDRESS;
+use crate::common_setup::setup_minter::open_edition_minter::minter_params::{
+    default_nft_data, init_msg,
+};
+use crate::common_setup::templates::open_edition_minter_custom_template;
 
-// const MINT_PRICE: u128 = 100_000_000;
+const MINT_PRICE: u128 = 100_000_000;
 
-// #[test]
-// fn check_mint_price_updates() {
-//     let vt =
-//         open_edition_minter_custom_template(None, None, None, Some(10), Some(2), None, None, None)
-//             .unwrap();
-//     let (mut router, creator, _buyer) = (vt.router, vt.accts.creator, vt.accts.buyer);
-//     let minter_addr = vt.collection_response_vec[0].minter.clone().unwrap();
+#[test]
+fn check_mint_price_updates() {
+    let params_extension = ParamsExtension {
+        max_per_address_limit: 10,
+        airdrop_mint_fee_bps: 100,
+        airdrop_mint_price: Coin {
+            denom: NATIVE_DENOM.to_string(),
+            amount: Uint128::new(100_000_000u128),
+        },
+        dev_fee_address: DEV_ADDRESS.to_string(),
+    };
+    let per_address_limit_minter = Some(2);
+    let init_msg = init_msg(
+        default_nft_data(),
+        per_address_limit_minter,
+        None,
+        None,
+        None,
+    );
 
-//     // Query Mint Price
-//     let query_mint_price_msg: QueryMsg = QueryMsg::MintPrice {};
-//     let res: open_edition_minter::msg::MintPriceResponse = router
-//         .wrap()
-//         .query_wasm_smart(minter_addr.clone(), &query_mint_price_msg)
-//         .unwrap();
-//     assert_eq!(
-//         res.current_price,
-//         Coin {
-//             denom: NATIVE_DENOM.to_string(),
-//             amount: Uint128::new(MINT_PRICE)
-//         }
-//     );
-//     assert_eq!(
-//         res.airdrop_price,
-//         Coin {
-//             denom: NATIVE_DENOM.to_string(),
-//             amount: Uint128::new(MINT_PRICE)
-//         }
-//     );
-//     assert_eq!(
-//         res.public_price,
-//         Coin {
-//             denom: NATIVE_DENOM.to_string(),
-//             amount: Uint128::new(MINT_PRICE)
-//         }
-//     );
+    let vt = open_edition_minter_custom_template(params_extension, init_msg).unwrap();
+    let (mut router, creator, _buyer) = (vt.router, vt.accts.creator, vt.accts.buyer);
+    let minter_addr = vt.collection_response_vec[0].minter.clone().unwrap();
 
-//     // Change to invalid price
-//     let update_msg = ExecuteMsg::UpdateMintPrice { price: 1u128 };
-//     let res = router.execute_contract(creator.clone(), minter_addr.clone(), &update_msg, &[]);
-//     assert_eq!(
-//         res.err().unwrap().source().unwrap().to_string(),
-//         "Minimum network mint price 100000000 got 1"
-//     );
+    // Query Mint Price
+    let query_mint_price_msg: QueryMsg = QueryMsg::MintPrice {};
+    let res: open_edition_minter::msg::MintPriceResponse = router
+        .wrap()
+        .query_wasm_smart(minter_addr.clone(), &query_mint_price_msg)
+        .unwrap();
+    assert_eq!(
+        res.current_price,
+        Coin {
+            denom: NATIVE_DENOM.to_string(),
+            amount: Uint128::new(MINT_PRICE)
+        }
+    );
 
-//     // Can increase the price because we are before the start time
-//     let update_msg = ExecuteMsg::UpdateMintPrice {
-//         price: MINT_PRICE + 100u128,
-//     };
-//     let res = router.execute_contract(creator.clone(), minter_addr.clone(), &update_msg, &[]);
-//     assert!(res.is_ok());
+    assert_eq!(
+        res.airdrop_price,
+        Coin {
+            denom: NATIVE_DENOM.to_string(),
+            amount: Uint128::new(MINT_PRICE)
+        }
+    );
+    assert_eq!(
+        res.public_price,
+        Coin {
+            denom: NATIVE_DENOM.to_string(),
+            amount: Uint128::new(MINT_PRICE)
+        }
+    );
 
-//     // Query the new Mint Price
-//     let query_mint_price_msg: QueryMsg = QueryMsg::MintPrice {};
-//     let res: open_edition_minter::msg::MintPriceResponse = router
-//         .wrap()
-//         .query_wasm_smart(minter_addr.clone(), &query_mint_price_msg)
-//         .unwrap();
-//     assert_eq!(
-//         res.current_price,
-//         Coin {
-//             denom: NATIVE_DENOM.to_string(),
-//             amount: Uint128::new(MINT_PRICE + 100u128)
-//         }
-//     );
-//     assert_eq!(
-//         res.airdrop_price,
-//         Coin {
-//             denom: NATIVE_DENOM.to_string(),
-//             amount: Uint128::new(MINT_PRICE)
-//         }
-//     );
-//     assert_eq!(
-//         res.public_price,
-//         Coin {
-//             denom: NATIVE_DENOM.to_string(),
-//             amount: Uint128::new(MINT_PRICE + 100u128)
-//         }
-//     );
+    // Change to invalid price
+    let update_msg = ExecuteMsg::UpdateMintPrice { price: 1u128 };
+    let res = router.execute_contract(creator.clone(), minter_addr.clone(), &update_msg, &[]);
+    assert_eq!(
+        res.err().unwrap().source().unwrap().to_string(),
+        "Minimum network mint price 100000000 got 1"
+    );
 
-//     // Set block forward, after start time. mint succeeds
-//     setup_block_time(&mut router, GENESIS_MINT_START_TIME + 200, None);
+    // Can increase the price because we are before the start time
+    let update_msg = ExecuteMsg::UpdateMintPrice {
+        price: MINT_PRICE + 100u128,
+    };
+    let res = router.execute_contract(creator.clone(), minter_addr.clone(), &update_msg, &[]);
+    assert!(res.is_ok());
 
-//     // Should not be able to increase price
-//     let update_msg = ExecuteMsg::UpdateMintPrice {
-//         price: MINT_PRICE + 200u128,
-//     };
-//     let res = router.execute_contract(creator.clone(), minter_addr.clone(), &update_msg, &[]);
-//     assert_eq!(
-//         res.err().unwrap().source().unwrap().to_string(),
-//         "Update price 100000200 higher than allowed price 100000100"
-//     );
+    // Query the new Mint Price
+    let query_mint_price_msg: QueryMsg = QueryMsg::MintPrice {};
+    let res: open_edition_minter::msg::MintPriceResponse = router
+        .wrap()
+        .query_wasm_smart(minter_addr.clone(), &query_mint_price_msg)
+        .unwrap();
+    assert_eq!(
+        res.current_price,
+        Coin {
+            denom: NATIVE_DENOM.to_string(),
+            amount: Uint128::new(MINT_PRICE + 100u128)
+        }
+    );
+    assert_eq!(
+        res.airdrop_price,
+        Coin {
+            denom: NATIVE_DENOM.to_string(),
+            amount: Uint128::new(MINT_PRICE)
+        }
+    );
+    assert_eq!(
+        res.public_price,
+        Coin {
+            denom: NATIVE_DENOM.to_string(),
+            amount: Uint128::new(MINT_PRICE + 100u128)
+        }
+    );
 
-//     // Decrease the price after the start time
-//     let update_msg = ExecuteMsg::UpdateMintPrice { price: MINT_PRICE };
-//     let res = router.execute_contract(creator.clone(), minter_addr.clone(), &update_msg, &[]);
-//     assert!(res.is_ok());
+    // Set block forward, after start time. mint succeeds
+    setup_block_time(&mut router, GENESIS_MINT_START_TIME + 200, None);
 
-//     // Query the new Mint Price
-//     let query_mint_price_msg: QueryMsg = QueryMsg::MintPrice {};
-//     let res: open_edition_minter::msg::MintPriceResponse = router
-//         .wrap()
-//         .query_wasm_smart(minter_addr.clone(), &query_mint_price_msg)
-//         .unwrap();
-//     assert_eq!(
-//         res.current_price,
-//         Coin {
-//             denom: NATIVE_DENOM.to_string(),
-//             amount: Uint128::new(MINT_PRICE)
-//         }
-//     );
-//     assert_eq!(
-//         res.airdrop_price,
-//         Coin {
-//             denom: NATIVE_DENOM.to_string(),
-//             amount: Uint128::new(MINT_PRICE)
-//         }
-//     );
-//     assert_eq!(
-//         res.public_price,
-//         Coin {
-//             denom: NATIVE_DENOM.to_string(),
-//             amount: Uint128::new(MINT_PRICE)
-//         }
-//     );
+    // Should not be able to increase price
+    let update_msg = ExecuteMsg::UpdateMintPrice {
+        price: MINT_PRICE + 200u128,
+    };
+    let res = router.execute_contract(creator.clone(), minter_addr.clone(), &update_msg, &[]);
+    assert_eq!(
+        res.err().unwrap().source().unwrap().to_string(),
+        "Update price 100000200 higher than allowed price 100000100"
+    );
 
-//     // If we are past the end time - cannot mint and cant change price either
-//     setup_block_time(&mut router, GENESIS_MINT_START_TIME + 50_000, None);
+    // Decrease the price after the start time
+    let update_msg = ExecuteMsg::UpdateMintPrice { price: MINT_PRICE };
+    let res = router.execute_contract(creator.clone(), minter_addr.clone(), &update_msg, &[]);
+    assert!(res.is_ok());
 
-//     // Try to change the price
-//     let update_msg = ExecuteMsg::UpdateMintPrice { price: MINT_PRICE };
-//     let res = router.execute_contract(creator, minter_addr, &update_msg, &[]);
-//     assert_eq!(
-//         res.err().unwrap().source().unwrap().to_string(),
-//         "Minting has ended"
-//     );
-// }
+    // Query the new Mint Price
+    let query_mint_price_msg: QueryMsg = QueryMsg::MintPrice {};
+    let res: open_edition_minter::msg::MintPriceResponse = router
+        .wrap()
+        .query_wasm_smart(minter_addr.clone(), &query_mint_price_msg)
+        .unwrap();
+    assert_eq!(
+        res.current_price,
+        Coin {
+            denom: NATIVE_DENOM.to_string(),
+            amount: Uint128::new(MINT_PRICE)
+        }
+    );
+    assert_eq!(
+        res.airdrop_price,
+        Coin {
+            denom: NATIVE_DENOM.to_string(),
+            amount: Uint128::new(MINT_PRICE)
+        }
+    );
+    assert_eq!(
+        res.public_price,
+        Coin {
+            denom: NATIVE_DENOM.to_string(),
+            amount: Uint128::new(MINT_PRICE)
+        }
+    );
+
+    // If we are past the end time - cannot mint and cant change price either
+    setup_block_time(&mut router, GENESIS_MINT_START_TIME + 50_000, None);
+
+    // Try to change the price
+    let update_msg = ExecuteMsg::UpdateMintPrice { price: MINT_PRICE };
+    let res = router.execute_contract(creator, minter_addr, &update_msg, &[]);
+    assert_eq!(
+        res.err().unwrap().source().unwrap().to_string(),
+        "Minting has ended"
+    );
+}
