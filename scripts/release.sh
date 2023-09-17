3668cad74c6b70aaccdffca09c7464b656216b57
Shane Vitarana <shanev@gmail.com>
2023-09-16 23:45:42 -0400

    Fix comments

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -eux
-# git checkout -b release-$1
+git checkout -b release-$1
 PROJECT_PATH=$(pwd)
-# version=$(grep "version" ./Cargo.toml | head -1 | cut -d '"' -f 2)
-# echo "Current version: $version"
+version=$(grep "version" ./Cargo.toml | head -1 | cut -d '"' -f 2)
+echo "Current version: $version"
 
-# echo "Replacing workspace version with $1 in manifest"
-# sed -i '' "s/version    = .*/version    = \""$1"\"/g" ./Cargo.toml
+echo "Replacing workspace version with $1 in manifest"
+sed -i '' "s/version    = .*/version    = \""$1"\"/g" ./Cargo.toml
 
-# echo "Replacing $version with $1 in manifest"
-# cd $PROJECT_PATH
-# sed -i '' "s/$version/$1/g" ./Cargo.toml
+echo "Replacing $version with $1 in manifest"
+cd $PROJECT_PATH
+sed -i '' "s/$version/$1/g" ./Cargo.toml
 
-# echo "Publishing packages"
-# . ./scripts/publish-packages.sh
+echo "Publishing packages"
+. ./scripts/publish-packages.sh
 
-# echo "Publishing contracts"
-# . ./scripts/publish-contracts.sh
+echo "Publishing contracts"
+. ./scripts/publish-contracts.sh
 
 echo "Generating schema"
 cd $PROJECT_PATH
