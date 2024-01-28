0b7e5685946cf0631bfbfa0acb32db4717ce6545
Shane Vitarana <shanev@gmail.com>
2024-01-28 16:37:38 -0500

    remove stargaze message wrapper

--- contracts/minters/vending-minter-tv/src/testing.rs
+++ contracts/minters/vending-minter-tv/src/testing.rs
@@ -1,23 +1,50 @@
-use cosmwasm_std::{coin, coins, Addr, Timestamp};
+use cosmwasm_std::{coin, coins, Empty, Timestamp};
 use cw_multi_test::Executor;
+use cw_multi_test::{App, BasicApp, Contract, ContractWrapper};
 use sg2::tests::mock_collection_params_1;
-use sg_multi_test::StargazeApp;
+// use sg_multi_test::StargazeApp;
+// use sg_std::StargazeMsgWrapper;
 use sg_std::{GENESIS_MINT_START_TIME, NATIVE_DENOM};
 use test_suite::common_setup::setup_accounts_and_block::setup_accounts;
+use test_suite::common_setup::setup_minter::vending_minter::mock_params::mock_params;
 use test_suite::common_setup::setup_minter::{
     common::constants::CREATION_FEE, vending_minter::mock_params::mock_init_extension,
-};
-use test_suite::common_setup::{
-    contract_boxes::contract_vending_factory,
-    setup_minter::vending_minter::mock_params::mock_params,
 };
 use vending_factory::msg::{
     TokenVaultVendingMinterCreateMsg, TokenVaultVendingMinterInitMsgExtension, VaultInfo,
 };
 
-pub fn custom_mock_app() -> StargazeApp {
-    StargazeApp::default()
+fn cw_vesting_contract() -> Box<dyn Contract<Empty>> {
+    let contract = ContractWrapper::new(
+        cw_vesting::contract::execute,
+        cw_vesting::contract::instantiate,
+        cw_vesting::contract::query,
+    );
+    Box::new(contract)
 }
+
+pub fn contract_vending_factory() -> Box<dyn Contract<Empty>> {
+    let contract = ContractWrapper::new(
+        vending_factory::contract::execute,
+        vending_factory::contract::instantiate,
+        vending_factory::contract::query,
+    )
+    .with_sudo(vending_factory::contract::sudo);
+    Box::new(contract)
+}
+
+// fn cw_vesting_contract() -> Box<dyn Contract<StargazeMsgWrapper>> {
+//     let contract = ContractWrapper::new(
+//         cw_vesting::contract::execute,
+//         cw_vesting::contract::instantiate,
+//         cw_vesting::contract::query,
+//     );
+//     Box::new(contract)
+// }
+
+// pub fn custom_mock_app() -> StargazeApp {
+//     StargazeApp::default()
+// }
 
 #[test]
 fn proper_initialization() {
@@ -25,8 +52,11 @@ fn proper_initialization() {
     let collection_params = mock_collection_params_1(Some(start_time));
     let params = mock_params(None);
 
-    let mut app = custom_mock_app();
+    // let mut app = custom_mock_app();
+    let mut app = BasicApp::default();
+
     let factory_code_id = app.store_code(contract_vending_factory());
+    let vesting_code_id = app.store_code(cw_vesting_contract());
     let (creator, buyer) = setup_accounts(&mut app);
 
     let factory_addr = app
