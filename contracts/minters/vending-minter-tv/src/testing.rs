f43fa3e7f0f35af0f9969b6b574310869d78cd2f
Shane Vitarana <shanev@gmail.com>
2024-02-01 19:21:44 -0500

    Refactor test setup

--- contracts/minters/vending-minter-tv/src/testing.rs
+++ contracts/minters/vending-minter-tv/src/testing.rs
@@ -1,11 +1,11 @@
-use cosmwasm_std::{coin, coins, Empty, Timestamp};
-use cw_multi_test::Executor;
-use cw_multi_test::{no_init, AppBuilder, Contract, ContractWrapper};
+use cosmwasm_std::{coin, coins, Addr, Empty, Timestamp};
+use cw_multi_test::{no_init, AppBuilder, BankSudo, Contract, ContractWrapper};
+use cw_multi_test::{Executor, SudoMsg};
 use sg2::tests::mock_collection_params_1;
 use sg_std::{GENESIS_MINT_START_TIME, NATIVE_DENOM};
 use test_suite::common_setup::contract_boxes::App;
 use test_suite::common_setup::keeper::StargazeKeeper;
-use test_suite::common_setup::setup_accounts_and_block::setup_accounts;
+use test_suite::common_setup::setup_accounts_and_block::INITIAL_BALANCE;
 use test_suite::common_setup::setup_minter::{
     common::constants::{
         AIRDROP_MINT_FEE_FAIR_BURN, AIRDROP_MINT_PRICE, CREATION_FEE, MAX_PER_ADDRESS_LIMIT,
@@ -18,11 +18,10 @@ use vending_factory::msg::{
 };
 use vending_factory::state::{ParamsExtension, VendingMinterParams};
 
-pub fn custom_mock_app() -> App {
-    AppBuilder::new()
-        .with_stargate(StargazeKeeper)
-        .build(no_init)
-}
+use crate::msg::ExecuteMsg;
+
+const FACTORY_ADMIN: &str = "factory_admin";
+const CREATOR: &str = "creator";
 
 fn cw_vesting_contract() -> Box<dyn Contract<Empty>> {
     let contract = ContractWrapper::new(
@@ -33,7 +32,7 @@ fn cw_vesting_contract() -> Box<dyn Contract<Empty>> {
     Box::new(contract)
 }
 
-pub fn contract_vending_factory() -> Box<dyn Contract<Empty>> {
+fn contract_vending_factory() -> Box<dyn Contract<Empty>> {
     let contract = ContractWrapper::new(
         vending_factory::contract::execute,
         vending_factory::contract::instantiate,
@@ -43,7 +42,7 @@ pub fn contract_vending_factory() -> Box<dyn Contract<Empty>> {
     Box::new(contract)
 }
 
-pub fn contract_vending_minter() -> Box<dyn Contract<Empty>> {
+fn contract_vending_minter() -> Box<dyn Contract<Empty>> {
     let contract = ContractWrapper::new(
         crate::contract::execute,
         crate::contract::instantiate,
@@ -53,13 +52,66 @@ pub fn contract_vending_minter() -> Box<dyn Contract<Empty>> {
     Box::new(contract)
 }
 
-pub fn contract_tv_collection() -> Box<dyn Contract<Empty>> {
+fn contract_tv_collection() -> Box<dyn Contract<Empty>> {
     let contract = ContractWrapper::new(
         sg721_tv::entry::execute,
         sg721_tv::entry::instantiate,
         sg721_tv::entry::query,
     );
     Box::new(contract)
+}
+
+fn setup_app() -> App {
+    let mut app = AppBuilder::new()
+        .with_stargate(StargazeKeeper)
+        .build(no_init);
+
+    app.sudo(SudoMsg::Bank({
+        BankSudo::Mint {
+            to_address: FACTORY_ADMIN.to_string(),
+            amount: coins(INITIAL_BALANCE, NATIVE_DENOM),
+        }
+    }))
+    .unwrap();
+
+    app.sudo(SudoMsg::Bank({
+        BankSudo::Mint {
+            to_address: CREATOR.to_string(),
+            amount: coins(CREATION_FEE, NATIVE_DENOM),
+        }
+    }))
+    .unwrap();
+
+    app
+}
+
+fn setup_contracts(app: &mut App) -> (Addr, u64, u64, u64) {
+    let factory_code_id = app.store_code(contract_vending_factory());
+    let vesting_code_id = app.store_code(cw_vesting_contract());
+    let vending_code_id = app.store_code(contract_vending_minter());
+    let collection_code_id = app.store_code(contract_tv_collection());
+
+    let mut params = mock_params(None);
+    params.code_id = vending_code_id;
+    params.allowed_sg721_code_ids = vec![collection_code_id];
+
+    let factory_addr = app
+        .instantiate_contract(
+            factory_code_id,
+            Addr::unchecked(FACTORY_ADMIN),
+            &vending_factory::msg::InstantiateMsg { params },
+            &[],
+            "factory",
+            None,
+        )
+        .unwrap();
+
+    (
+        factory_addr,
+        vesting_code_id,
+        vending_code_id,
+        collection_code_id,
+    )
 }
 
 pub fn mock_params(mint_denom: Option<String>) -> VendingMinterParams {
@@ -86,33 +138,9 @@ pub fn mock_params(mint_denom: Option<String>) -> VendingMinterParams {
 
 #[test]
 fn proper_initialization() {
-    let mut app = custom_mock_app();
+    let mut app = setup_app();
 
-    let factory_code_id = app.store_code(contract_vending_factory());
-    let vesting_code_id = app.store_code(cw_vesting_contract());
-    let vending_code_id = app.store_code(contract_vending_minter());
-    let collection_code_id = app.store_code(contract_tv_collection());
-
-    let start_time = Timestamp::from_nanos(GENESIS_MINT_START_TIME);
-    let mut collection_params = mock_collection_params_1(Some(start_time));
-    collection_params.code_id = collection_code_id;
-
-    let (creator, buyer) = setup_accounts(&mut app);
-
-    let mut params = mock_params(None);
-    params.code_id = vending_code_id;
-    params.allowed_sg721_code_ids = vec![collection_code_id];
-
-    let factory_addr = app
-        .instantiate_contract(
-            factory_code_id,
-            creator.clone(),
-            &vending_factory::msg::InstantiateMsg { params },
-            &[],
-            "factory",
-            None,
-        )
-        .unwrap();
+    let (factory_addr, vesting_code_id, _, collection_code_id) = setup_contracts(&mut app);
 
     let base = mock_init_extension(None, None);
 
@@ -126,6 +154,10 @@ fn proper_initialization() {
 
     let init_msg = TokenVaultVendingMinterInitMsgExtension { base, vault_info };
 
+    let start_time = Timestamp::from_nanos(GENESIS_MINT_START_TIME);
+    let mut collection_params = mock_collection_params_1(Some(start_time));
+    collection_params.code_id = collection_code_id;
+
     let create_minter_msg = TokenVaultVendingMinterCreateMsg {
         init_msg,
         collection_params,
@@ -135,12 +167,26 @@ fn proper_initialization() {
 
     let creation_fee = coins(CREATION_FEE, NATIVE_DENOM);
 
-    let res = app.execute_contract(creator, factory_addr.clone(), &msg, &creation_fee);
+    let res = app.execute_contract(
+        Addr::unchecked(CREATOR),
+        factory_addr.clone(),
+        &msg,
+        &creation_fee,
+    );
 
     assert!(res.is_ok())
 }
 
-#[test]
-fn mint() {
-    proper_initialization();
-}
+// #[test]
+// fn mint() {
+//     proper_initialization();
+
+//     let mint_msg = ExecuteMsg::Mint {};
+//     let res = router.execute_contract(
+//         buyer.clone(),
+//         minter_addr.clone(),
+//         &mint_msg,
+//         &coins(MINT_PRICE, NATIVE_DENOM),
+//     );
+//     assert!(res.is_ok());
+// }
