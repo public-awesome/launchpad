44c75d2a1780730e4cc412e11f0c3806cc0e21c4
Shane Vitarana <shanev@gmail.com>
2024-02-11 22:02:48 -0500

    disable vesting tests for now

--- contracts/cw-vesting-owned/src/lib.rs
+++ contracts/cw-vesting-owned/src/lib.rs
@@ -15,9 +15,9 @@ pub use cw_ownable::Ownership;
 // so consumers don't need a cw_stake_tracker dependency to use this contract's queries.
 pub use cw_stake_tracker::StakeTrackerQuery;
 
-#[cfg(test)]
-mod suite_tests;
-#[cfg(test)]
-mod tests;
-#[cfg(test)]
-mod vesting_tests;
+// #[cfg(test)]
+// mod suite_tests;
+// #[cfg(test)]
+// mod tests;
+// #[cfg(test)]
+// mod vesting_tests;
