df372234b35a9dc3f1e6c51a991e402092aba466
Shane Vitarana <shanev@gmail.com>
2025-08-18 14:40:27 -0400

    Updated tests

--- test-suite/src/vending_minter/tests/contract_whitelist.rs
+++ test-suite/src/vending_minter/tests/contract_whitelist.rs
@@ -3,37 +3,36 @@ use cosmwasm_std::{coins, Addr};
 use cw_multi_test::Executor;
 use sg_utils::{GENESIS_MINT_START_TIME, NATIVE_DENOM};
 use vending_minter::msg::ExecuteMsg;
-use vending_minter::msg::{IsContractWhitelistedResponse, QueryMsg, WhitelistedContractsResponse};
-use vending_minter::ContractError;
+use vending_factory::msg::{IsContractWhitelistedResponse, QueryMsg as FactoryQueryMsg, SudoMsg, WhitelistedContractsResponse};
 
 use crate::common_setup::setup_accounts_and_block::setup_block_time;
 
 const MINT_PRICE: u128 = 100_000_000;
 
 #[test]
-fn test_admin_can_add_contract_to_whitelist() {
+fn test_governance_can_add_contract_to_factory_whitelist() {
     let vt = vending_minter_template(1);
     let (mut router, creator, _buyer) = (vt.router, vt.accts.creator, vt.accts.buyer);
-    let minter_addr = vt.collection_response_vec[0].minter.clone().unwrap();
+    let factory_addr = vt.collection_response_vec[0].factory.clone().unwrap();
 
     // Create a fake contract address
     let contract_addr = "contract1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghij";
 
-    // Admin should be able to add contract to whitelist
-    let add_msg = ExecuteMsg::AddContractToWhitelist {
+    // Use sudo to add contract to factory whitelist (governance-only operation)
+    let sudo_msg = SudoMsg::AddContractToWhitelist {
         address: contract_addr.to_string(),
     };
 
-    let res = router.execute_contract(creator, minter_addr.clone(), &add_msg, &[]);
-    assert!(res.is_ok(), "Admin should be able to add contract to whitelist");
+    let res = router.wasm_sudo(factory_addr.clone(), &sudo_msg);
+    assert!(res.is_ok(), "Governance should be able to add contract to factory whitelist");
 
-    // Check that the contract is now whitelisted
-    let query_msg = QueryMsg::IsContractWhitelisted {
+    // Check that the contract is now whitelisted via factory query
+    let query_msg = FactoryQueryMsg::IsContractWhitelisted {
         address: contract_addr.to_string(),
     };
     let res: IsContractWhitelistedResponse = router
         .wrap()
-        .query_wasm_smart(minter_addr, &query_msg)
+        .query_wasm_smart(factory_addr, &query_msg)
         .unwrap();
 
     assert!(res.is_whitelisted, "Contract should be whitelisted");
@@ -41,34 +40,30 @@ fn test_admin_can_add_contract_to_whitelist() {
 }
 
 #[test]
-fn test_non_admin_cannot_add_contract_to_whitelist() {
+fn test_minter_no_longer_has_whitelist_execute_handlers() {
     let vt = vending_minter_template(1);
-    let (mut router, _creator, buyer) = (vt.router, vt.accts.creator, vt.accts.buyer);
-    let minter_addr = vt.collection_response_vec[0].minter.clone().unwrap();
+    let (_router, _creator, _buyer) = (vt.router, vt.accts.creator, vt.accts.buyer);
+    let _minter_addr = vt.collection_response_vec[0].minter.clone().unwrap();
 
-    let contract_addr = "contract1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghij";
+    let _contract_addr = "contract1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghij";
 
-    // Non-admin should not be able to add contract to whitelist
-    let add_msg = ExecuteMsg::AddContractToWhitelist {
-        address: contract_addr.to_string(),
-    };
-
-    let res = router.execute_contract(buyer, minter_addr, &add_msg, &[]);
-    assert!(res.is_err(), "Non-admin should not be able to add to whitelist");
-
-    let err = res.unwrap_err();
-    let contract_err = err.downcast_ref::<ContractError>().unwrap();
-    assert!(
-        matches!(contract_err, ContractError::Unauthorized(_)),
-        "Should return Unauthorized error"
-    );
+    // These execute messages should no longer exist on the minter
+    // This test should fail to compile, proving the migration worked
+    // Commenting out to allow compilation
+    
+    // let add_msg = ExecuteMsg::AddContractToWhitelist {
+    //     address: contract_addr.to_string(),
+    // };
+    // let res = router.execute_contract(creator, minter_addr, &add_msg, &[]);
+    // assert!(res.is_err(), "Minter should no longer have whitelist execute handlers");
 }
 
 #[test]
 fn test_whitelisted_contract_can_mint() {
     let vt = vending_minter_template(2); // Need 2 tokens for this test
-    let (mut router, creator, _buyer) = (vt.router, vt.accts.creator, vt.accts.buyer);
+    let (mut router, _creator, _buyer) = (vt.router, vt.accts.creator, vt.accts.buyer);
     let minter_addr = vt.collection_response_vec[0].minter.clone().unwrap();
+    let factory_addr = vt.collection_response_vec[0].factory.clone().unwrap();
 
     // Create a long contract address (would normally be blocked)
     let contract_addr = Addr::unchecked("contract1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghij");
@@ -96,12 +91,12 @@ fn test_whitelisted_contract_can_mint() {
     );
     assert!(res.is_err(), "Non-whitelisted contract should be blocked");
 
-    // Admin adds contract to whitelist
-    let add_msg = ExecuteMsg::AddContractToWhitelist {
+    // Governance adds contract to factory whitelist (sudo operation)
+    let sudo_msg = SudoMsg::AddContractToWhitelist {
         address: contract_addr.to_string(),
     };
-    let res = router.execute_contract(creator, minter_addr.clone(), &add_msg, &[]);
-    assert!(res.is_ok(), "Admin should be able to add contract to whitelist");
+    let res = router.wasm_sudo(factory_addr, &sudo_msg);
+    assert!(res.is_ok(), "Governance should be able to add contract to factory whitelist");
 
     // Now minting should succeed
     let res = router.execute_contract(
@@ -114,128 +109,128 @@ fn test_whitelisted_contract_can_mint() {
 }
 
 #[test]
-fn test_remove_contract_from_whitelist() {
+fn test_remove_contract_from_factory_whitelist() {
     let vt = vending_minter_template(1);
-    let (mut router, creator, _buyer) = (vt.router, vt.accts.creator, vt.accts.buyer);
-    let minter_addr = vt.collection_response_vec[0].minter.clone().unwrap();
+    let (mut router, _creator, _buyer) = (vt.router, vt.accts.creator, vt.accts.buyer);
+    let factory_addr = vt.collection_response_vec[0].factory.clone().unwrap();
 
     let contract_addr = "contract1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghij";
 
-    // Add contract to whitelist
-    let add_msg = ExecuteMsg::AddContractToWhitelist {
+    // Add contract to factory whitelist via sudo
+    let add_msg = SudoMsg::AddContractToWhitelist {
         address: contract_addr.to_string(),
     };
     router
-        .execute_contract(creator.clone(), minter_addr.clone(), &add_msg, &[])
+        .wasm_sudo(factory_addr.clone(), &add_msg)
         .unwrap();
 
     // Verify it's whitelisted
-    let query_msg = QueryMsg::IsContractWhitelisted {
+    let query_msg = FactoryQueryMsg::IsContractWhitelisted {
         address: contract_addr.to_string(),
     };
     let res: IsContractWhitelistedResponse = router
         .wrap()
-        .query_wasm_smart(minter_addr.clone(), &query_msg)
+        .query_wasm_smart(factory_addr.clone(), &query_msg)
         .unwrap();
     assert!(res.is_whitelisted);
 
-    // Remove from whitelist
-    let remove_msg = ExecuteMsg::RemoveContractFromWhitelist {
+    // Remove from whitelist via sudo
+    let remove_msg = SudoMsg::RemoveContractFromWhitelist {
         address: contract_addr.to_string(),
     };
-    let res = router.execute_contract(creator, minter_addr.clone(), &remove_msg, &[]);
-    assert!(res.is_ok(), "Admin should be able to remove from whitelist");
+    let res = router.wasm_sudo(factory_addr.clone(), &remove_msg);
+    assert!(res.is_ok(), "Governance should be able to remove from factory whitelist");
 
     // Verify it's no longer whitelisted
     let res: IsContractWhitelistedResponse = router
         .wrap()
-        .query_wasm_smart(minter_addr, &query_msg)
+        .query_wasm_smart(factory_addr, &query_msg)
         .unwrap();
     assert!(!res.is_whitelisted, "Contract should no longer be whitelisted");
 }
 
 #[test]
-fn test_batch_update_contract_whitelist() {
+fn test_batch_update_factory_contract_whitelist() {
     let vt = vending_minter_template(1);
-    let (mut router, creator, _buyer) = (vt.router, vt.accts.creator, vt.accts.buyer);
-    let minter_addr = vt.collection_response_vec[0].minter.clone().unwrap();
+    let (mut router, _creator, _buyer) = (vt.router, vt.accts.creator, vt.accts.buyer);
+    let factory_addr = vt.collection_response_vec[0].factory.clone().unwrap();
 
     let contract1 = "contract1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefgh1";
     let contract2 = "contract1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefgh2";
     let contract3 = "contract1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefgh3";
 
-    // Add contract1 to whitelist first
-    let add_msg = ExecuteMsg::AddContractToWhitelist {
+    // Add contract1 to factory whitelist first via sudo
+    let add_msg = SudoMsg::AddContractToWhitelist {
         address: contract1.to_string(),
     };
     router
-        .execute_contract(creator.clone(), minter_addr.clone(), &add_msg, &[])
+        .wasm_sudo(factory_addr.clone(), &add_msg)
         .unwrap();
 
     // Batch update: add contract2 and contract3, remove contract1
-    let batch_msg = ExecuteMsg::UpdateContractWhitelist {
+    let batch_msg = SudoMsg::UpdateContractWhitelist {
         add: vec![contract2.to_string(), contract3.to_string()],
         remove: vec![contract1.to_string()],
     };
 
-    let res = router.execute_contract(creator, minter_addr.clone(), &batch_msg, &[]);
-    assert!(res.is_ok(), "Admin should be able to batch update whitelist");
+    let res = router.wasm_sudo(factory_addr.clone(), &batch_msg);
+    assert!(res.is_ok(), "Governance should be able to batch update factory whitelist");
 
     // Check results
-    let query_msg1 = QueryMsg::IsContractWhitelisted {
+    let query_msg1 = FactoryQueryMsg::IsContractWhitelisted {
         address: contract1.to_string(),
     };
     let res1: IsContractWhitelistedResponse = router
         .wrap()
-        .query_wasm_smart(minter_addr.clone(), &query_msg1)
+        .query_wasm_smart(factory_addr.clone(), &query_msg1)
         .unwrap();
     assert!(!res1.is_whitelisted, "Contract1 should be removed");
 
-    let query_msg2 = QueryMsg::IsContractWhitelisted {
+    let query_msg2 = FactoryQueryMsg::IsContractWhitelisted {
         address: contract2.to_string(),
     };
     let res2: IsContractWhitelistedResponse = router
         .wrap()
-        .query_wasm_smart(minter_addr.clone(), &query_msg2)
+        .query_wasm_smart(factory_addr.clone(), &query_msg2)
         .unwrap();
     assert!(res2.is_whitelisted, "Contract2 should be added");
 
-    let query_msg3 = QueryMsg::IsContractWhitelisted {
+    let query_msg3 = FactoryQueryMsg::IsContractWhitelisted {
         address: contract3.to_string(),
     };
     let res3: IsContractWhitelistedResponse = router
         .wrap()
-        .query_wasm_smart(minter_addr, &query_msg3)
+        .query_wasm_smart(factory_addr, &query_msg3)
         .unwrap();
     assert!(res3.is_whitelisted, "Contract3 should be added");
 }
 
 #[test]
-fn test_query_whitelisted_contracts() {
+fn test_query_factory_whitelisted_contracts() {
     let vt = vending_minter_template(1);
-    let (mut router, creator, _buyer) = (vt.router, vt.accts.creator, vt.accts.buyer);
-    let minter_addr = vt.collection_response_vec[0].minter.clone().unwrap();
+    let (mut router, _creator, _buyer) = (vt.router, vt.accts.creator, vt.accts.buyer);
+    let factory_addr = vt.collection_response_vec[0].factory.clone().unwrap();
 
     let contract1 = "contract1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefgh1";
     let contract2 = "contract1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefgh2";
 
-    // Add both contracts to whitelist
-    let batch_msg = ExecuteMsg::UpdateContractWhitelist {
+    // Add both contracts to factory whitelist via sudo
+    let batch_msg = SudoMsg::UpdateContractWhitelist {
         add: vec![contract1.to_string(), contract2.to_string()],
         remove: vec![],
     };
     router
-        .execute_contract(creator, minter_addr.clone(), &batch_msg, &[])
+        .wasm_sudo(factory_addr.clone(), &batch_msg)
         .unwrap();
 
-    // Query all whitelisted contracts
-    let query_msg = QueryMsg::WhitelistedContracts {
+    // Query all whitelisted contracts from factory
+    let query_msg = FactoryQueryMsg::WhitelistedContracts {
         start_after: None,
         limit: None,
     };
     let res: WhitelistedContractsResponse = router
         .wrap()
-        .query_wasm_smart(minter_addr, &query_msg)
+        .query_wasm_smart(factory_addr, &query_msg)
         .unwrap();
 
     assert_eq!(res.contracts.len(), 2, "Should have 2 whitelisted contracts");
